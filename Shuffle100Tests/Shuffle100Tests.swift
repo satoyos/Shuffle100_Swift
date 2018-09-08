5e9a959e9185e468f65cac6af16c6551bca4b173
Yosifumi Sato <sato_yos@nifty.com>
2018-09-08 22:19:11 +0900

    Introduce Unit test for HomeViewController

--- Shuffle100Tests/Shuffle100Tests.swift
+++ Shuffle100Tests/Shuffle100Tests.swift
@@ -21,16 +21,12 @@ class Shuffle100Tests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_InitScreen() {
+        let screen = HomeViewController()
+        XCTAssertNotNil(screen)
+        screen.viewDidLoad()
+        XCTAssertEqual(screen.navigationItem.title, "百首読み上げ")
+        XCTAssertEqual(screen.view.backgroundColor, UIColor.white)
     }
     
 }
