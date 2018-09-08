5e0a029f22af3304ccad4e57a93bad6ec42d1f7c
Yosifumi Sato <sato_yos@nifty.com>
2018-09-08 17:08:44 +0900

    Initial Commit

---
+++ Shuffle100/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shuffle100
+//
+//  Created by 里 佳史 on 2018/09/08.
+//  Copyright © 2018年 里 佳史. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
