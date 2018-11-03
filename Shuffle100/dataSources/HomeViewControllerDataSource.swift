344c5c29b9a9b32dcced8c02f5e95e78edead183
Yosifumi Sato <sato_yos@nifty.com>
2018-11-03 20:54:31 +0900

    Set accessory for each cell ionHomeScreenTableView

--- Shuffle100/dataSources/HomeViewControllerDataSource.swift
+++ Shuffle100/dataSources/HomeViewControllerDataSource.swift
@@ -20,9 +20,11 @@ struct TableSection {
 
 struct DataSource {
     var title: String
+    var accessoryType: UITableViewCell.AccessoryType
     
-    init(title: String) {
+    init(title: String, cellType type: UITableViewCell.AccessoryType) {
         self.title = title
+        self.accessoryType = type
     }
 }
 
@@ -31,17 +33,12 @@ class HomeScreenTableCell: UITableViewCell {
     
     func configure(dataSource: DataSource) {
         textLabel?.text = dataSource.title
-        accessoryType = .disclosureIndicator
+        accessoryType = dataSource.accessoryType
     }
 }
 
 class HomeViewControllerDataSource: NSObject, UITableViewDataSource {
     fileprivate var sections = [TableSection]()
-    
-    override init() {
-        super.init()
-        self.setupDataSources()
-    }
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return sections.count
@@ -49,8 +46,8 @@ class HomeViewControllerDataSource: NSObject, UITableViewDataSource {
     
     //    ToDo: このDataSourceプロトコルをsetup()メソッドで DataSourceを使って実装していく
 
-    func setupDataSources() {
-        sections.append(settingSection())
+    func setupDataSources(withTypes types: [HomeCellType]) {
+        sections.append(settingSection(withTypes: types))
         sections.append(gameStartSection())
     }
     
@@ -63,26 +60,49 @@ class HomeViewControllerDataSource: NSObject, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: HomeScreenTableCell.identifier) as! UITableViewCell
-
-        cell.textLabel?.text = sections[indexPath.section].dataSources[indexPath.row].title
+        let cell = tableView.dequeueReusableCell(withIdentifier: HomeScreenTableCell.identifier, for: indexPath)
+        if let cell = cell as? HomeScreenTableCell {
+            cell.configure(dataSource: sections[indexPath.section].dataSources[indexPath.row])
+        } 
         return cell
     }
     
-    fileprivate func settingSection() -> TableSection {
+    fileprivate func settingSection(withTypes types: [HomeCellType]) -> TableSection {
         var section = TableSection(title: "設定")
-        section.dataSources.append(DataSource(title: "取り札を用意する歌"))
-        section.dataSources.append(DataSource(title: "読み上げモード"))
-        section.dataSources.append(DataSource(title: "初心者モード"))
-        section.dataSources.append(DataSource(title: "読手"))
-        
+        for type in types {
+            switch type {
+            case .poems:
+                section.dataSources.append(poemsDataSource())
+            case .reciteMode:
+                section.dataSources.append(reciteModeDataSource())
+            case .beginnerMode:
+                section.dataSources.append(beginnerModeDataSource())
+            case .singers:
+                section.dataSources.append(singerDataSource())
+            }
+        }
         return section
     }
     
     fileprivate func gameStartSection() -> TableSection {
         var section = TableSection(title: "試合開始")
-        section.dataSources.append(DataSource(title: "試合開始"))
+        section.dataSources.append(DataSource(title: "試合開始", cellType: .none))
         return section
     }
     
+    fileprivate func poemsDataSource() -> DataSource {
+        return DataSource(title: "取り札を用意する歌", cellType: .disclosureIndicator)
+    }
+    
+    fileprivate func reciteModeDataSource() -> DataSource {
+        return DataSource(title: "読み上げモード", cellType: .disclosureIndicator)
+    }
+    
+    fileprivate func beginnerModeDataSource() -> DataSource {
+        return DataSource(title: "初心者モード", cellType: .none)
+    }
+    
+    fileprivate func singerDataSource() -> DataSource {
+        return DataSource(title: "読手", cellType: .disclosureIndicator)
+    }
 }
--- Shuffle100/screens/HomeViewController.swift
+++ Shuffle100/screens/HomeViewController.swift
@@ -8,17 +8,24 @@
 
 import UIKit
 
+enum HomeCellType {
+    case poems
+    case reciteMode
+    case beginnerMode
+    case singers
+}
+
 class HomeViewController: UIViewController {
     let titleName = "トップ"
     var tableView: UITableView!
     var dataSource = HomeViewControllerDataSource()
-//    var delegate = HomeViewControllerDelegate()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = titleName
         self.view.backgroundColor = UIColor.white
         self.tableView = UITableView(frame: view.bounds, style: .grouped)
+        dataSource.setupDataSources(withTypes: homeCells())
         tableView.dataSource = dataSource
         tableView.delegate = self
         tableView.register(HomeScreenTableCell.self, forCellReuseIdentifier: "HomeScreenTableCell")
@@ -30,6 +37,9 @@ class HomeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    private func homeCells() -> [HomeCellType] {
+        return [.poems, .reciteMode, .beginnerMode, .singers]
+    }
 }
 
 extension HomeViewController: UITableViewDelegate {
