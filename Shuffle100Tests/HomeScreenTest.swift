95d5d9185003a3ecb9d505c50890425f16e9ed90
Yosifumi Sato <sato_yos@nifty.com>
2019-03-04 21:31:46 +0900

    Cut out some Unit tests about default HomeScreen to extension file

--- Shuffle100Tests/HomeScreenTest.swift
+++ Shuffle100Tests/HomeScreenTest.swift
@@ -22,53 +22,8 @@ class HomeScreenTest: XCTestCase {
         super.tearDown()
     }
     
-    func test_InitialHomeScreen() {
-        XCTAssertEqual(screen.navigationItem.title, "トップ")
-        XCTAssertEqual(screen.view.backgroundColor, UIColor.white)
-    }
-    
-    private func startGameCell() -> GameStartCell {
+    internal func startGameCell() -> GameStartCell {
         return screen.tableView(screen.tableView, cellForRowAt: IndexPath(row: 0, section: 1)) as! GameStartCell
-    }
-    
-    func test_allCellsHaveAccessibilityLabel() {
-        let cell_top = screen.tableView(screen.tableView, cellForRowAt: IndexPath(row: 0, section: 0))
-        XCTAssertEqual(cell_top.accessibilityLabel, "poemsCell")
-        
-        let cell_2nd = screen.tableView(screen.tableView, cellForRowAt: IndexPath(row: 1, section: 0))
-        XCTAssertEqual(cell_2nd.accessibilityLabel, "reciteModeCell")
-        
-        let cell_startGame = startGameCell()
-        XCTAssertEqual(cell_startGame.accessibilityLabel, "GameStartCell")
-    }
-
-    func test_startGameCellTextIsRed() {
-        let cell_startGame = startGameCell()
-        XCTAssertEqual(cell_startGame.textLabel?.textColor, UIColor.red)
-    }
-    
-    func test_startGameCellStyleIsDefault() {
-        let cell_startGame = startGameCell()
-        XCTAssertEqual(cell_startGame.cellStyle, UITableViewCell.CellStyle.default)
-    }
-   
-    func test_fakeModeCellHasSwitch_defaultIsOff() {
-        let fakeModeCell = screen.tableView(screen.tableView, cellForRowAt: IndexPath(row: 2, section: 0))
-        guard let switchView = fakeModeCell.accessoryView as? UISwitch else {
-            XCTAssert(false)
-            return
-        }
-        XCTAssertFalse(switchView.isOn)
-    }
-    
-    func test_defaultReciteModeIsNormal() {
-        let defaultReciteMode = screen.gameSettings.reciteMode
-        XCTAssertEqual(defaultReciteMode, .normal)
-    }
-
-    func test_defaultReciteModeLabelIsNormal() {
-        let reciteModeCell = screen.tableView(screen.tableView, cellForRowAt: IndexPath(row: 1, section: 0))
-        XCTAssertEqual(reciteModeCell.detailTextLabel?.text, "通常")
     }
     
     func test_beginnerModeLabelIsBeginner() {
