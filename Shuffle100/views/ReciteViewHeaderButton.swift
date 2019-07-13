6b12a24a0999535341c59d0b8e369930d0611f9f
Yosifumi Sato <sato_yos@nifty.com>
2019-07-13 18:16:24 +0900

    Introduce ReciteViewHeaderButton: buttons for header part of RecitePoemScreen

---
+++ Shuffle100/views/ReciteViewHeaderButton.swift
@@ -0,0 +1,27 @@
+//
+//  ReciteViewHeaderButton.swift
+//  Shuffle100
+//
+//  Created by 里 佳史 on 2019/07/13.
+//  Copyright © 2019 里 佳史. All rights reserved.
+//
+
+import UIKit
+
+class ReciteViewHeaderButton: UIButton {
+
+    var tappedAction:  ( ()->Void )?
+    
+    func setAction(action: @escaping () -> Void) {
+        tappedAction = action
+        self.addTarget(self, action: #selector(tapped), for: .touchUpInside)
+    }
+    
+    @objc func tapped(btn: UIButton) {
+        guard let action = tappedAction else {
+            print("No action is set to this button!")
+            return
+        }
+        action()
+    }
+}
