ba31b92ade258e60153af13b8d466e29ff121238
Yosifumi Sato <sato_yos@nifty.com>
2019-07-14 15:13:50 +0900

    [WIP] Just add UITest when Gama start: the test fails at this time

---
+++ Shuffle100UITests/UITestWIthMultiScreen/GotoRecitePoemScreenUITest.swift
@@ -0,0 +1,29 @@
+//
+//  GotoRecitePoemScreenUITest.swift
+//  Shuffle100UITests
+//
+//  Created by 里 佳史 on 2019/07/14.
+//  Copyright © 2019 里 佳史. All rights reserved.
+//
+
+import XCTest
+
+class GotoRecitePoemScreenUITest: XCTestCase {
+    var app = XCUIApplication()
+
+    override func setUp() {
+        continueAfterFailure = false
+        app.launch()
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func test_RecitePoemScreenAppearWhenGameStartCellTapped() {
+        // when
+        app.tables.cells["GameStartCell"].tap()
+        // then
+        XCTAssert(app.staticTexts["序歌"].exists)
+    }
+}
