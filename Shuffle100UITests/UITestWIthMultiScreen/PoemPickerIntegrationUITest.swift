4bd785cdf108f975c3b3d7f950993a77c98b64a5
Yosifumi Sato <sato_yos@nifty.com>
2019-07-21 21:48:16 +0900

    Refactor UITest for PoemPickerScreen

--- Shuffle100UITests/UITestWIthMultiScreen/PoemPickerIntegrationUITest.swift
+++ Shuffle100UITests/UITestWIthMultiScreen/PoemPickerIntegrationUITest.swift
@@ -24,27 +24,37 @@ class PoemPickerIntegrationUITest: XCTestCase {
     
     
     func test_HomeScreenReflectsSelectioninPoemPicker() {
-        let tablesQuery = app.tables
-
+        
         XCTContext.runActivity(named: "デフォルトのトップ画面に「100首」と書かれたセルが存在する") { (activity) in
             XCTAssertTrue(app.cells.staticTexts["100首"].exists)
         }
-        XCTContext.runActivity(named: "「取り札を用意する歌」セルをタップすると、歌選択画面に遷移する") { (activity) in
-            tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["取り札を用意する歌"]/*[[".cells[\"poemsCell\"].staticTexts[\"取り札を用意する歌\"]",".staticTexts[\"取り札を用意する歌\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-            XCTAssert(app.navigationBars["歌を選ぶ"].exists)
-        }
+        gotoPoemPickerScreenTest()
         XCTContext.runActivity(named: "プレースホルダ付きの検索窓がある") { (activity) in
             XCTAssert(app.searchFields["歌を検索"].exists)
         }
         XCTContext.runActivity(named: "歌を3つタップして選択状態を解除し、トップ画面に戻ると、歌の数が97首になっている") { (activity) in
             //   tap poems #1,2, 4
-            tablesQuery.cells["001"].tap()
-            tablesQuery.cells["002"].tap()
-            tablesQuery.cells["004"].tap()
+            app.tables.cells["001"].tap()
+            app.tables.cells["002"].tap()
+            app.tables.cells["004"].tap()
             //   back to HomeScreen
             app.navigationBars["歌を選ぶ"].buttons["トップ"].tap()
             // then
             XCTAssertTrue(app.cells.staticTexts["97首"].exists)
+        }
+    }
+    
+//    func test_searchPoem() {
+//        // given
+//        let tableQuery = app.tables
+//        // when
+//        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["取り札を用意する歌"]/*[[".cells[\"poemsCell\"].staticTexts[\"取り札を用意する歌\"]",".staticTexts[\"取り札を用意する歌\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+//    }
+    
+    func gotoPoemPickerScreenTest() {
+        XCTContext.runActivity(named: "「取り札を用意する歌」セルをタップすると、歌選択画面に遷移する") { (activity) in
+            app.tables/*@START_MENU_TOKEN@*/.staticTexts["取り札を用意する歌"]/*[[".cells[\"poemsCell\"].staticTexts[\"取り札を用意する歌\"]",".staticTexts[\"取り札を用意する歌\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+            XCTAssert(app.navigationBars["歌を選ぶ"].exists)
         }
     }
 }
