fb50797945aa8eae96a536c1509bfca3a9648b5d
Yosifumi Sato <sato_yos@nifty.com>
2019-07-24 22:17:27 +0900

    Introduce "Singer" data type to manage singers metadata

---
+++ Shuffle100/models/Singer.swift
@@ -0,0 +1,34 @@
+//
+//  Singer.swift
+//  Shuffle100
+//
+//  Created by 里 佳史 on 2019/07/24.
+//  Copyright © 2019 里 佳史. All rights reserved.
+//
+
+import Foundation
+
+struct Singer: Codable {
+    let id: String
+    let path: String
+    let name: String
+}
+
+struct Singers {
+    static var all = readSingersFromJson()
+    
+    static func readSingersFromJson() -> [Singer] {
+        let jsonPath = Bundle.main.path(forResource: "Singers", ofType: "json")!
+        if let jsonData = try? Data(contentsOf: URL(fileURLWithPath: jsonPath)) {
+            let decoder = JSONDecoder()
+            if let singers = try? decoder.decode([Singer].self, from: jsonData) {
+                return singers
+            } else {
+                fatalError("読手のJSONデータのデコードに失敗")
+            }
+        } else {
+            fatalError("読手のJSONデータの読み込みに失敗")
+        }
+    }
+    
+}
---
+++ Shuffle100Tests/models/SingerTests.swift
@@ -0,0 +1,28 @@
+//
+//  SingerTests.swift
+//  Shuffle100Tests
+//
+//  Created by 里 佳史 on 2019/07/24.
+//  Copyright © 2019 里 佳史. All rights reserved.
+//
+
+import XCTest
+
+class SingerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func test_SingersStructHas2SingerStructs() {
+        XCTAssertEqual(Singers.all.count, 2)
+        let first = Singers.all.first
+        XCTAssertEqual(first?.id, "ia")
+        let second = Singers.all[1]
+        XCTAssertEqual(second.path, "audio/inaba")
+    }
+}
