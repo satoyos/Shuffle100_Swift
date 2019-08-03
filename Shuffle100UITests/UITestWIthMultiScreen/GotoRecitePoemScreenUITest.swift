04de88a758556dd468e7d39c48b023357cd64157
Yosifumi Sato <sato_yos@nifty.com>
2019-08-03 21:05:34 +0900

    Caught problem (UITest failure only in Xcode Server CI), and remove useless trial code to avoid failure

--- Shuffle100UITests/UITestWIthMultiScreen/GotoRecitePoemScreenUITest.swift
+++ Shuffle100UITests/UITestWIthMultiScreen/GotoRecitePoemScreenUITest.swift
@@ -21,29 +21,33 @@ class GotoRecitePoemScreenUITest: XCTestCase {
     }
 
     func test_RecitePoemScreenAppearWhenGameStartCellTapped() {
-        // when
-        app.tables.cells["GameStartCell"].tap()
-        // then
-        //
-        sleep(2)
-        //
-        XCTAssert(app.staticTexts["序歌"].exists)
+        gotoRecitePoemScreenFromHome()
     }
     
     func test_backToHomeScreenUsingExitButton() {
+        // given
+        gotoRecitePoemScreenFromHome()
+        
+        XCTContext.runActivity(named: "Exitボタンを押すと、確認のダイアログが現れる") { (activiti) in
+            // when
+            app.buttons["exit"].tap()
+            // then
+            XCTAssert(app.alerts.staticTexts["試合を終了しますか？"].exists)
+        }
+        
+        XCTContext.runActivity(named: "確認ダイアログで「終了する」を押すと、トップ画面に戻る") {
+            (activity) in
+            // when
+            app.alerts.buttons["終了する"].tap()
+            // then
+            XCTAssert(app.navigationBars["トップ"].exists)
+        }
+    }
+    
+    private func gotoRecitePoemScreenFromHome() {
         // when
         app.tables.cells["GameStartCell"].tap()
-        //
-        sleep(2)
-        //
-        app.buttons["exit"].tap()
         // then
-//        XCTAssert(app.alerts.staticTexts["試合を終了しますか？"].exists)
-        waitToAppear(for: app.alerts.staticTexts["試合を終了しますか？"])
-        // when
-        app.alerts.buttons["終了する"].tap()
-        // then
-        waitToAppear(for: app.navigationBars["トップ"])
-        XCTAssert(app.navigationBars["トップ"].exists)        
+        XCTAssert(app.staticTexts["序歌"].exists)
     }
 }
