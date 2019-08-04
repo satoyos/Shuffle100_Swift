93430f0ebe991c09c6f2de1655d1fdad22b13dd8
Yosifumi Sato <sato_yos@nifty.com>
2019-08-04 19:51:23 +0900

    Introduce Coordinator protocol

---
+++ Shuffle100/coordinaters/CoordinatorProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  CoordinatorProtocol.swift
+//  Shuffle100
+//
+//  Created by 里 佳史 on 2019/08/04.
+//  Copyright © 2019 里 佳史. All rights reserved.
+//
+
+import Foundation
+
+protocol Coordinator {
+    func start()
+}
