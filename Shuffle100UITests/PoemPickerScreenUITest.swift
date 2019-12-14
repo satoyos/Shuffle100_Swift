56324a24011fe6aaef4d26665853ec17fc57b3ad
Yosifumi Sato <sato_yos@nifty.com>
2019-12-14 22:51:15 +0900

    Set action for "Cancel All" button on PoemPickerScreen

---
+++ Shuffle100UITests/PoemPickerScreenUITest.swift
@@ -0,0 +1,45 @@
+//
+//  PoemPickerScreenUITest.swift
+//  Shuffle100UITests
+//
+//  Created by 里 佳史 on 2019/12/14.
+//  Copyright © 2019 里 佳史. All rights reserved.
+//
+
+import XCTest
+
+class PoemPickerScreenUITest: XCTestCase {
+    let app = XCUIApplication()
+    
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        app.launchEnvironment = ["IS_TESTING" : "1"]
+        app.launch()
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func test_cancelAllAndSelectAll() {
+        XCTContext.runActivity(named: "「全て取消」ボタンを押すと、選ばれている歌が0首になる") { activity in
+            // given
+            gotoPoemPickerScreen()
+            // when
+            sleep(1)
+            app.buttons["全て取消"].tap()
+            app.navigationBars["歌を選ぶ"].buttons["トップ"].tap()
+            // then
+            XCTAssertTrue(app.cells.staticTexts["0首"].exists)
+        }
+    }
+    
+    private func gotoPoemPickerScreen() {
+        // when
+        app.tables/*@START_MENU_TOKEN@*/.staticTexts["取り札を用意する歌"]/*[[".cells[\"poemsCell\"].staticTexts[\"取り札を用意する歌\"]",".staticTexts[\"取り札を用意する歌\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        // then
+        XCTAssert(app.staticTexts["歌を選ぶ"].exists)
+    }
+
+}
