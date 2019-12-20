7d5d123df9691d9b13a9d548d03c1f74546968f2
Yosifumi Sato <sato_yos@nifty.com>
2019-12-20 21:44:44 +0900

    Add Unit test for RecitePoemView

---
+++ Shuffle100Tests/views/RecitePoemViewTests.swift
@@ -0,0 +1,23 @@
+//
+//  RecitePoemViewTests.swift
+//  RecitePoemViewTests
+//
+//  Created by 里 佳史 on 2019/07/05.
+//  Copyright © 2019 里 佳史. All rights reserved.
+//
+
+import XCTest
+
+class RecitePoemViewTests: XCTestCase {
+
+    class RecitePoemViewTest: XCTestCase {
+        
+        func test_initRcitePoemView() {
+            let view = RecitePoemView()
+            view.initView(title: "test")
+            XCTAssertNotNil(view)
+        }
+        
+    }
+
+}
