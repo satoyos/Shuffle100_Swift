d02f99f013fe17591a995cac5443ba503d681a73
Yosifumi Sato <sato_yos@nifty.com>
2019-12-20 22:10:38 +0900

    Just introduced GameEndView for RecitePoemScreen

---
+++ Shuffle100Tests/views/GameEndViewTest.swift
@@ -0,0 +1,28 @@
+//
+//  GameEndViewTest.swift
+//  Shuffle100Tests
+//
+//  Created by 里 佳史 on 2019/12/20.
+//  Copyright © 2019 里 佳史. All rights reserved.
+//
+
+import XCTest
+
+class GameEndViewTest: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func test_initGameEndView() {
+        let view = GameEndViiew()
+        view.initView(title: "test title")
+        XCTAssertEqual(view.backToHomeButton.titleLabel?.text, "トップに戻る")
+    }
+
+
+}
