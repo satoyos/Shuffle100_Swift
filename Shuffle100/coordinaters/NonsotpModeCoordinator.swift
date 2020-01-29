25046de79976fc45527fa55bcdc3b8d94f9650c9
Yoshifumi Sato <sato_yos@nifty.com>
2020-01-29 22:12:55 +0900

    Refactor: move common method to super class AGAIN

--- Shuffle100/coordinaters/NonsotpModeCoordinator.swift
+++ Shuffle100/coordinaters/NonsotpModeCoordinator.swift
@@ -10,20 +10,6 @@ import UIKit
 
 class NonsotpModeCoordinator: RecitePoemCoordinator {
 
-//    //
-//    // 問題なければ、このメソッドはSuper classいに移す！
-//    //
-//    override internal func jokaFinished() {
-//        print("序歌の読み上げ終了!!")
-//        guard let firstPoem = poemSupplier.drawNextPoem() else { return }
-//        let number = firstPoem.number
-//        let counter = poemSupplier.currentIndex
-//        screen!.playerFinishedAction = { [weak self, number, counter] in
-//            self?.reciteKamiFinished(number: number, counter: counter)
-//        }
-//        screen!.stepIntoNextPoem(number: number, at: counter, total: poemSupplier.size)
-//    }
-    
     override internal func reciteKamiFinished(number: Int, counter: Int ) {
         print("\(counter)番めの歌(歌番号: \(number))の上の句の読み上げ終了。(ノンストップ)")
         stepIntoShimoInNonstopMode()
@@ -37,21 +23,5 @@ class NonsotpModeCoordinator: RecitePoemCoordinator {
             self?.reciteShimoFinished(number: number, counter: counter)
         }
         screen?.slideIntoShimo(number: number, at: counter, total: poemSupplier.size)
-    }
-    
-    private func reciteShimoFinished(number: Int, counter: Int) {
-        print("\(counter)番めの歌(歌番号: \(number))の下の句の読み上げ終了。")
-        if let poem = poemSupplier.drawNextPoem() {
-            let number = poem.number
-            let counter = poemSupplier.currentIndex
-            screen!.playerFinishedAction = { [weak self, number, counter] in
-                self?.reciteKamiFinished(number: number, counter: counter)
-            }
-            screen!.stepIntoNextPoem(number: number, at: counter, total: poemSupplier.size)
-        
-        } else {
-            print("歌は全て読み終えた！")
-            screen!.stepIntoGameEnd()
-        }
     }
 }
