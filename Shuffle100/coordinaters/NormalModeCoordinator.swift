641b9bfa3657cf3dcdd90132d4ef0d78a884316b
Yoshifumi Sato <sato_yos@nifty.com>
2020-02-11 20:40:45 +0900

    Forward Skip button works at the end of Kami in Normal reciting mode.

--- Shuffle100/coordinaters/NormalModeCoordinator.swift
+++ Shuffle100/coordinaters/NormalModeCoordinator.swift
@@ -11,21 +11,24 @@ import UIKit
 final class NormalModeCoordinator: RecitePoemCoordinator {
 
     override internal func reciteKamiFinished(number: Int, counter: Int ) {
-        print("\(counter)番めの歌(歌番号: \(number))の上の句の読み上げ終了。")
+//        print("\(counter)番めの歌(歌番号: \(number))の上の句の読み上げ終了。")
         guard let screen = screen else { return }
         screen.waitUserActionAfterFineshdReciing()
         screen.playButtonTappedAfterFinishedReciting = { [weak self] in
             self?.stepIntoShimoInNormalMode()
         }
+        screen.skipToNextScreenAction = { [weak self] in
+            self?.stepIntoShimoInNormalMode()
+        }
     }
   
     private func stepIntoShimoInNormalMode() {
-        print("上の句が終わった状態でPlayButtonが押された！")
+        guard let screen = self.screen else { return }
         let number = poemSupplier.poem.number
         let counter = poemSupplier.currentIndex
-        screen!.playerFinishedAction = { [weak self, number, counter] in
+        screen.playerFinishedAction = { [weak self, number, counter] in
             self?.reciteShimoFinished(number: number, counter: counter)
         }
-        screen?.slideIntoShimo(number: number, at: counter, total: poemSupplier.size)
+        screen.slideIntoShimo(number: number, at: counter, total: poemSupplier.size)
     }
 }
