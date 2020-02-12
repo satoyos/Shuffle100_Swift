9ce5498a04f5ec9aa84016f1c7d40951dbbb36a9
Yoshifumi Sato <sato_yos@nifty.com>
2020-02-12 22:09:31 +0900

    Just introduced SelectSingerScreen

---
+++ Shuffle100/screens/SelectSingerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SelectSingerViewController.swift
+//  Shuffle100
+//
+//  Created by Yoshifumi Sato on 2020/02/12.
+//  Copyright © 2020 里 佳史. All rights reserved.
+//
+
+import UIKit
+
+class SelectSingerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "読手を選ぶ"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
