1c1fcbf388bbd175d6b2b2c4a4e3770130f934ae
Yoshifumi Sato <sato_yos@nifty.com>
2020-02-22 18:25:40 +0900

    Cut Out make Settings part from MainCoordinator as its extension

---
+++ Shuffle100/coordinaters/MainCoordinatorSetUpSettings.swift
@@ -0,0 +1,65 @@
+//
+//  MainCoordinatorSetUpSettings.swift
+//  Shuffle100
+//
+//  Created by Yoshifumi Sato on 2020/02/22.
+//  Copyright © 2020 里 佳史. All rights reserved.
+//
+
+import Foundation
+
+extension MainCoordinator {
+    internal func setUpSettings() -> Settings {
+        let defaultSettings = Settings()
+        if let loadedSettings = store.load(key: Settings.userDefaultKey) as Settings? {
+            if env.ignoreSavedData() {
+                return defaultSettings
+            } else {
+                return loadedSettings
+            }
+        } else {
+            if let gameSettings = tryLoadLegacyGameSettings() {
+                if !env.ignoreSavedData() {
+                    initSettings(defaultSettings, with: gameSettings)
+                }
+            }
+            if let recitingSettings = tryLoadLegacyRecitingSettings() {
+                if !env.ignoreSavedData() {
+                    initSettings(defaultSettings, with: recitingSettings)
+                }
+            }
+            return defaultSettings
+        }
+    }
+    
+    private func tryLoadLegacyRecitingSettings() -> RecitingSettings? {
+        if let loadedSettings = RecitingSettings.salvageDataFromUserDefaults() {
+            print("+++ Success loading Legacy Data")
+            loadedSettings.debugPrint()
+            return loadedSettings
+        } else {
+            return nil
+        }
+    }
+    
+    private func tryLoadLegacyGameSettings() -> GameSettings? {
+        if let gameSettings = GameSettings.salvageDataFromUserDefaults() {
+            print("+++ Success loading legacy GameSettings")
+            gameSettings.debugPrint()
+            return gameSettings
+        } else {
+            return nil
+        }
+    }
+    
+    private func initSettings(_ settings: Settings, with gameSettings: GameSettings) {
+        settings.fakeMode = gameSettings.fake_flg
+        settings.reciteMode = .normal // still fail loading legacy saved symbol data
+        settings.state100 = LegacyDataConverter.state100FromGameSettings(gameSettings)
+        settings.savedFudaSets = LegacyDataConverter.savedFudaSetsFromGameSettings(gameSettings)
+    }
+    
+    private func initSettings(_ settings: Settings, with recitingSettings: RecitingSettings) {
+        settings.recitingConfig = LegacyDataConverter.convertRecitingSettings(recitingSettings)
+    }
+}
