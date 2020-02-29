03a643d4adc4a9a2ca2c11f6f43674a65f6afdc1
Yoshifumi Sato <sato_yos@nifty.com>
2020-02-29 22:26:19 +0900

    Add util method for UITest
     - and give up UITest for UserDefault at this time

--- Shuffle100UITests/utils/SelectSingerScreenUITestUtils.swift
+++ Shuffle100UITests/utils/SelectSingerScreenUITestUtils.swift
@@ -9,12 +9,23 @@
 import Foundation
 import XCTest
 
+private let singerNames = ["IA（ボーカロイド）", "いなばくん（人間）"]
+
 protocol SelectSingerScreenUITestUtils {
     func goBackToHomeScreen(_ app: XCUIApplication) -> Void
+    func selectSingerFor(name: String, in app: XCUIApplication) -> Void
 }
 
 extension SelectSingerScreenUITestUtils {
     func goBackToHomeScreen(_ app: XCUIApplication) {
         app.navigationBars["読手を選ぶ"].buttons["トップ"].tap()
+    }
+    
+    func selectSingerFor(name: String, in app: XCUIApplication) {
+        if let formalName = singerNames.first(where: { $0.contains(name) }) {
+            app.pickerWheels.element.adjust(toPickerWheelValue: formalName)
+        } else {
+            XCTFail("与えられた文字列を含む毒手が見つからない！")
+        }
     }
 }
