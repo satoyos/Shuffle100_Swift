50c477d852eda7aec36727584735aea4ae812a28
Yoshifumi Sato <sato_yos@nifty.com>
2020-03-01 20:13:38 +0900

    Just introduce ReciteSettingsScreen

---
+++ Shuffle100/screens/ReciteSettingsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ReciteSettingsViewController.swift
+//  Shuffle100
+//
+//  Created by Yoshifumi Sato on 2020/03/01.
+//  Copyright © 2020 里 佳史. All rights reserved.
+//
+
+import UIKit
+
+class ReciteSettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "いろいろな設定"
+    }
+
+}
