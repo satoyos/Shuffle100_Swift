4785ee9d26b76f58c9a5fe6fbb9e49e802c0ddfe
Yoshifumi Sato <sato_yos@nifty.com>
2020-03-04 23:09:35 +0900

    Set title of cells on ReciteSettingsScreen

--- Shuffle100/screens/ReciteSettingsViewController.swift
+++ Shuffle100/screens/ReciteSettingsViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import Then
 
 class ReciteSettingsViewController: UITableViewController {
+    let reuseID = "ReciteSettings"
     var settings: Settings!
+    let settingNames = ["歌と歌の間隔", "上の句と下の句の間隔", "音量調整"]
 
     init(settings: Settings = Settings()) {
         self.settings = settings
@@ -27,11 +30,32 @@ class ReciteSettingsViewController: UITableViewController {
         super.viewDidLoad()
 
         self.title = "いろいろな設定"
-        self.view.backgroundColor = .white
+        view.backgroundColor = .white
+        self.tableView = createTableViewForReciteSettingsScreen()
         
     }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return settingNames.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseID, for: indexPath).then {
+            let name = settingNames[indexPath.row]
+            $0.textLabel?.text = name
+        }
+        return cell
+    }
+    
+    private func createTableViewForReciteSettingsScreen() -> UITableView {
+        let tableView = UITableView(frame: view.bounds, style: .grouped)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseID)
+        return tableView
     }
 }
