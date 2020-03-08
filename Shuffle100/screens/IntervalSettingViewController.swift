589e6d85db2ee16c39437e31afcb73c7c6aa821b
Yoshifumi Sato <sato_yos@nifty.com>
2020-03-08 21:45:46 +0900

    Just introduce IntervalSettingScreen

---
+++ Shuffle100/screens/IntervalSettingViewController.swift
@@ -0,0 +1,21 @@
+//
+//  IntervalSettingViewController.swift
+//  Shuffle100
+//
+//  Created by Yoshifumi Sato on 2020/03/08.
+//  Copyright © 2020 里 佳史. All rights reserved.
+//
+
+import UIKit
+
+class IntervalSettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "歌の間隔の調整"
+    }
+    
+
+
+}
