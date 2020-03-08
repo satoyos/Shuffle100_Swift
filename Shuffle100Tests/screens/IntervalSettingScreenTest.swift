3e262aadef6d55f099ab0a9ee471cca06fd8161b
Yoshifumi Sato <sato_yos@nifty.com>
2020-03-08 21:45:46 +0900

    Just introduce IntervalSettingScreen

---
+++ Shuffle100Tests/screens/IntervalSettingScreenTest.swift
@@ -0,0 +1,33 @@
+//
+//  IntervalSettingScreenTest.swift
+//  Shuffle100Tests
+//
+//  Created by Yoshifumi Sato on 2020/03/08.
+//  Copyright © 2020 里 佳史. All rights reserved.
+//
+
+import XCTest
+@testable import Shuffle100
+
+class IntervalSettingScreenTest: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func test_initialScreen() {
+        // given, when
+        let screen = IntervalSettingViewController()
+        // then
+        XCTAssertNotNil(screen)
+        // when
+        screen.loadViewIfNeeded()
+        // then
+        XCTAssertEqual(screen.title, "歌の間隔の調整")
+        
+    }
+}
