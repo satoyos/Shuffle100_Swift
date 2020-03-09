11cb8b27eee13206714f07cc460ae8aa1464d5be
Yoshifumi Sato <sato_yos@nifty.com>
2020-03-09 22:47:01 +0900

    Just introduce Superclass for screen with settings: SettingsAttachedViewController

--- Shuffle100/screens/IntervalSettingViewController.swift
+++ Shuffle100/screens/IntervalSettingViewController.swift
@@ -8,20 +8,7 @@
 
 import UIKit
 
-class IntervalSettingViewController: UIViewController {
-    var settings: Settings!
-    
-    init(settings: Settings = Settings()) {
-        self.settings = settings
-
-        // クラスの持つ指定イニシャライザを呼び出す
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    // 新しく init を定義した場合に必須
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
+class IntervalSettingViewController: SettingsAttachedViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Shuffle100/screens/SettingsAttachedViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SettingsAttachedViewController.swift
+//  Shuffle100
+//
+//  Created by Yoshifumi Sato on 2020/03/09.
+//  Copyright © 2020 里 佳史. All rights reserved.
+//
+
+import UIKit
+
+class SettingsAttachedViewController: UIViewController {
+
+    var settings: Settings!
+    
+    init(settings: Settings = Settings()) {
+        self.settings = settings
+
+        // クラスの持つ指定イニシャライザを呼び出す
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    // 新しく init を定義した場合に必須
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
