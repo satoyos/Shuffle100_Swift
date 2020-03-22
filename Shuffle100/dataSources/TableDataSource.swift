ded3bb1c9e49c700bda2c51e2be3ce42e49beb54
Yoshifumi Sato <sato_yos@nifty.com>
2020-03-22 22:59:02 +0900

    Cut out TableDataSource class  from HomeScreenDataSource.swift

--- Shuffle100/dataSources/TableDataSource.swift
+++ Shuffle100/dataSources/TableDataSource.swift
@@ -1,5 +1,5 @@
 //
-//  HomeViewControllerDataSource.swift
+//  TableDataSource.swift
 //  Shuffle100
 //
 //  Created by 里 佳史 on 2018/10/06.
@@ -7,6 +7,11 @@
 //
 
 import UIKit
+
+//
+// Going to cut out from here
+//
+
 
 struct TableSection {
     var title: String
@@ -30,58 +35,5 @@ struct TableDataSource {
         self.title = title
         self.accessoryType = type
         self.detailLabelText = detailLabelText
-    }
-}
-
-extension HomeViewController: UITableViewDataSource {
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return sections.count
-    }
-
-    func setupDataSources(withTypes types: [HomeCellType]) {
-        self.sections = [TableSection]()
-        sections.append(settingSection(withTypes: types))
-        sections.append(gameStartSection())
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return sections[section].dataSources.count
-    }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return sections[section].title
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.section == 0 {
-            let cell = tableView.dequeueReusableCell(withIdentifier: HomeScreenTableCell.identifier) as! HomeScreenTableCell
-            
-            cell.configure(dataSource: sections[indexPath.section].dataSources[indexPath.row])
-            if let switchView = cell.accessoryView as? UISwitch {
-                switchView.addTarget(self, action: #selector(switchValueChanged) , for: .valueChanged)
-            }
-            return cell
-        } else {
-            let cell = tableView.dequeueReusableCell(withIdentifier: GameStartCell.identifier, for: indexPath) as! GameStartCell
-            
-            cell.configure(dataSource: sections[indexPath.section].dataSources[indexPath.row])
-            return cell
-        }
-    }
-    
-    private func settingSection(withTypes types: [HomeCellType]) -> TableSection {
-        var section = TableSection(title: "設定")
-        section.dataSources = types.map{
-            HomeScreenDataSourceFactory.settingsDataSource(for: $0, settings: settings)}
-        return section
-    }
-    
-    private func gameStartSection() -> TableSection {
-        var section = TableSection(title: "試合開始")
-        var dataSource = HomeScreenDataSourceFactory.startGameDataSource()
-        dataSource.accessibilityLabel = GameStartCell.identifier
-        section.dataSources = [dataSource]
-        return section
     }
 }
