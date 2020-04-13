23267d4722d271c694a250d023328aafd6b16614
Yoshifumi Sato <sato_yos@nifty.com>
2020-04-13 22:12:01 +0900

    [WIP] Just introduce KamiShimoIntervalSettingScreen

---
+++ Shuffle100/screens/KamiShimoIntervalSettingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  KamiShimoIntervalSettingViewController.swift
+//  Shuffle100
+//
+//  Created by Yoshifumi Sato on 2020/04/13.
+//  Copyright © 2020 里 佳史. All rights reserved.
+//
+
+import UIKit
+
+class KamiShimoIntervalSettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
