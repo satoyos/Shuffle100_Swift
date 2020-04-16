c15720783dbb87a5ee30335dbcfcd27fbed744fe
Yoshifumi Sato <sato_yos@nifty.com>
2020-04-16 22:41:29 +0900

    make KamiShimoIntervalSettingViewController subclass of TimeSettingViewController

--- Shuffle100/screens/KamiShimoIntervalSettingViewController.swift
+++ Shuffle100/screens/KamiShimoIntervalSettingViewController.swift
@@ -7,46 +7,13 @@
 //
 
 import UIKit
-import SnapKit
 
-class KamiShimoIntervalSettingViewController: UIViewController {
-    let timeLabel = UILabel()
-    let slider = UISlider()
-    private let sizeByDevice = SizeFactory.createSizeByDevice()
-    var tryButton = UIButton()
+class KamiShimoIntervalSettingViewController: TimeSettingViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.title = "上の句と下の句の間隔"
-        view.backgroundColor = StandardColor.backgroundColor
-        view.addSubview(timeLabel)
-        configureTimeLabel()
     }
     
-    private func configureTimeLabel() {
-        _ = timeLabel.then {
-            $0.text = "0.00"
-            $0.font = UIFont.systemFont(ofSize: labelPointSize())
-            $0.sizeToFit()
-            $0.snp.makeConstraints{ (make) -> Void in
-                // Center => [50%, 40%]
-                make.centerX.equalToSuperview()
-                make.centerY.equalToSuperview().offset(-1 * one10thOfViewHeight())
-            }
-        }
-    }
-    
-    private func labelPointSize() -> CGFloat {
-        return sizeByDevice.intervalTimeLabelPointSize()
-    }
-    
-    private func one10thOfViewHeight() -> CGFloat {
-        return 0.1 * viewHeiht()
-    }
-    
-    private func viewHeiht() -> CGFloat {
-        return view.frame.size.height
-    }
-
 }
