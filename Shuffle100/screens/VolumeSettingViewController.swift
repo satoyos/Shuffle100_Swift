3b1e684d232d4748bbb1ddeedf5854cafaf099c4
Yoshifumi Sato <sato_yos@nifty.com>
2020-04-18 12:01:41 +0900

    Just introduce VolumeSettingScreen

---
+++ Shuffle100/screens/VolumeSettingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  VolumeSettingViewController.swift
+//  Shuffle100
+//
+//  Created by Yoshifumi Sato on 2020/04/18.
+//  Copyright © 2020 里 佳史. All rights reserved.
+//
+
+import UIKit
+
+class VolumeSettingViewController: SettingsAttachedViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
