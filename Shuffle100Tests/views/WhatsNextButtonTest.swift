afc52202cc3cd3a33e85cb4a83ed8052bd37ef4b
Yoshifumi Sato <sato_yos@nifty.com>
2020-04-29 11:50:23 +0900

    Just introduce WhatsNextButton

--- Shuffle100Tests/views/WhatsNextButtonTest.swift
+++ Shuffle100Tests/views/WhatsNextButtonTest.swift
@@ -1,14 +1,15 @@
 //
-//  WhatsNextScreenTest.swift
+//  WhatsNextButtonTest.swift
 //  Shuffle100Tests
 //
-//  Created by Yoshifumi Sato on 2020/04/27.
+//  Created by Yoshifumi Sato on 2020/04/29.
 //  Copyright © 2020 里 佳史. All rights reserved.
 //
 
 import XCTest
+@testable import Shuffle100
 
-class WhatsNextScreenTest: XCTestCase {
+class WhatsNextButtonTest: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -18,14 +19,11 @@ class WhatsNextScreenTest: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func test_initialScreen() {
-        // given
-        let screen = WhatsNextViewController()
-        // when
-        screen.loadViewIfNeeded()
-        // then
-        XCTAssertEqual(screen.title, "次はどうする？")
-        XCTAssertEqual(screen.view.subviews.count, 3)
+    func test_canBeInitialized() {
+        let button = WhatsNextButton()
+        button.frame.size = CGSize(width: 100, height: 100)
+        button.initWithImage(filename: "refrain.png")
+        XCTAssertNotNil(button)
     }
 
 }
