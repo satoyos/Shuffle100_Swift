2fa0dd0f6fa157d0425b37a309f8b4e016d46a5c
Yoshifumi Sato <sato_yos@nifty.com>
2020-05-06 15:33:44 +0900

    cut out gotoWhatsNextScreen to common Protocol

--- Shuffle100UITests/utils/RecitePoemScreenUITestUtils.swift
+++ Shuffle100UITests/utils/RecitePoemScreenUITestUtils.swift
@@ -8,10 +8,11 @@
 
 import XCTest
 
-protocol RecitePoemScreenUITestUtils {
+protocol RecitePoemScreenUITestUtils: HomeScreenUITestUtils {
     func tapForwardButton(_ app: XCUIApplication) -> Void
     func tapRewindButton(_ app: XCUIApplication) -> Void
     func tapPlayButton(_ app: XCUIApplication) -> Void
+    func gotoWhatsNextScreen(_ app: XCUIApplication, poemsNumber:Int) -> Void
 }
 
 extension RecitePoemScreenUITestUtils {
@@ -25,5 +26,33 @@ extension RecitePoemScreenUITestUtils {
     
     func tapPlayButton(_ app: XCUIApplication) {
         app.buttons["play"].tap()
+    }
+    
+    func gotoWhatsNextScreen(_ app: XCUIApplication, poemsNumber:Int = 100) {
+        XCTContext.runActivity(named: "初心者モードを選択") { (activity) in
+            // when
+            gotoSelectModeScreen(app)
+            app.pickerWheels.element.adjust(toPickerWheelValue: "初心者 (チラし取り)")
+            app.buttons["トップ"].tap()
+            // then
+            XCTAssert(app.cells.staticTexts["初心者"].exists)
+        }
+        XCTContext.runActivity(named: "そして序歌へ") { (activity) in
+            gotoRecitePoemScreen(app)
+        }
+        XCTContext.runActivity(named: "forwardボタンを押すと、1首めの上の句へ") { (activity) in
+            tapForwardButton(app)
+            XCTAssert(app.staticTexts["1首め:上の句 (全\(poemsNumber)首)"].exists)
+        }
+        XCTContext.runActivity(named: "上の句の読み上げ後、自動的に下の句へ") { (activity) in
+            
+            tapForwardButton(app)
+            XCTAssert(app.staticTexts["1首め:下の句 (全\(poemsNumber)首)"].exists)
+        }
+        XCTContext.runActivity(named: "下の句が終わると、「次はどうする？」画面が現れる") { (activity) in
+            
+            tapForwardButton(app)
+            XCTAssert(app.staticTexts["次はどうする？"].exists)
+        }
     }
 }
