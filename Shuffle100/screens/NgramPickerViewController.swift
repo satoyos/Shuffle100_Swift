fb24e5a43e29f2f847998453637e313c373fca46
Yoshifumi Sato <sato_yos@nifty.com>
2020-05-08 19:03:16 +0900

    [WIP] just introduce NgramPickerScreen

---
+++ Shuffle100/screens/NgramPickerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NgramPickerViewController.swift
+//  Shuffle100
+//
+//  Created by Yoshifumi Sato on 2020/05/08.
+//  Copyright © 2020 里 佳史. All rights reserved.
+//
+
+import UIKit
+
+class NgramPickerViewController: SettingsAttachedViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "1字目で選ぶ"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Shuffle100Tests/screens/NgramPickerScreenTest.swift
@@ -0,0 +1,33 @@
+//
+//  NgramPickerScreenTest.swift
+//  Shuffle100Tests
+//
+//  Created by Yoshifumi Sato on 2020/05/08.
+//  Copyright © 2020 里 佳史. All rights reserved.
+//
+
+import XCTest
+@testable import Shuffle100
+
+class NgramPickerScreenTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func test_initialScreen() throws {
+        // given
+        let screen = NgramPickerViewController()
+        // when
+        screen.loadViewIfNeeded()
+        // then
+        XCTAssertEqual(screen.title, "1字目で選ぶ")
+        
+    }
+
+    
+}
