a5948bdeb6ffbd512320db1107cc28550ee5f033
Yoshifumi Sato <sato_yos@nifty.com>
2020-06-06 21:51:34 +0900

    Refactor UI test code to reuse code around Ngram

--- Shuffle100UITests/utils/PoemPickerScreenUITestUtils.swift
+++ Shuffle100UITests/utils/PoemPickerScreenUITestUtils.swift
@@ -9,12 +9,26 @@
 import Foundation
 import XCTest
 
-protocol PoemPickerScreenUITestUtils {
+protocol PoemPickerScreenUITestUtils: WaitInUITest {
     func goBackToHomeScreen(_ app: XCUIApplication) -> Void
+    func gotoNgramPickerScreenFromPickerScreen(_ app: XCUIApplication)
 }
 
 extension PoemPickerScreenUITestUtils {
     func goBackToHomeScreen(_ app: XCUIApplication) {
         app.navigationBars["歌を選ぶ"].buttons["トップ"].tap()
+    }
+    
+    func gotoNgramPickerScreenFromPickerScreen(_ app: XCUIApplication) {
+        // given
+        let button = waitToHittable(for: app.buttons["まとめて選ぶ"], timeout: 3)
+        // when
+        button.tap()
+        // then
+        XCTAssert(app.staticTexts["どうやって選びますか？"].exists)
+        // when
+        app.buttons["1字目で選ぶ"].tap()
+        // then
+        waitToAppear(for: app.navigationBars["1字目で選ぶ"], timeout: 5)
     }
 }
