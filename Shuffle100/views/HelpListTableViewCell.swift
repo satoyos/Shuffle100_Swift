c2e9a9c134cb196966fa5b5708692a78ff11ff31
Yoshifumi Sato <sato_yos@nifty.com>
2020-06-21 21:39:27 +0900

    Introduce dataSource for HelpListScreen

---
+++ Shuffle100/views/HelpListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HelpListTableViewCell.swift
+//  Shuffle100
+//
+//  Created by Yoshifumi Sato on 2020/06/21.
+//  Copyright © 2020 里 佳史. All rights reserved.
+//
+
+import UIKit
+
+class HelpListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
