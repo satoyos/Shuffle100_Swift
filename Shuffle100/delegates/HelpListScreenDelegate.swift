53d1700b4576f9b194fe1498b662ff351bff4ab8
Yoshifumi Sato <sato_yos@nifty.com>
2020-06-21 23:16:37 +0900

    Introduce delegate part of HelpListScreen

---
+++ Shuffle100/delegates/HelpListScreenDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  HelpListScreenDelegate.swift
+//  Shuffle100
+//
+//  Created by Yoshifumi Sato on 2020/06/21.
+//  Copyright © 2020 里 佳史. All rights reserved.
+//
+
+import UIKit
+
+extension HelpListViewController: UITableViewDelegate {
+    
+}
