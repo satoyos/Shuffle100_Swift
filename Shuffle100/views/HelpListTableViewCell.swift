8a78505be294ad52650c68b17d7a59bb2a4a7a37
Yoshifumi Sato <sato_yos@nifty.com>
2020-06-24 23:41:45 +0900

    Configure HelpListTableViewCell with HelpListDataSrouce

--- Shuffle100/views/HelpListTableViewCell.swift
+++ Shuffle100/views/HelpListTableViewCell.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class HelpListTableViewCell: UITableViewCell {
+    private var htmlFileName: String!
 
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .value1, reuseIdentifier: reuseIdentifier)
+       }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -19,6 +28,17 @@ class HelpListTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    func configure(with dataSource: HelpListDataSource) {
+        self.textLabel?.text = dataSource.name
+        switch dataSource.type {
+        case .html:
+            self.htmlFileName = dataSource.fileName
+            self.accessoryType = .disclosureIndicator
+        case .value1:
+            self.detailTextLabel?.text = dataSource.detail
+        }
     }
 
 }
