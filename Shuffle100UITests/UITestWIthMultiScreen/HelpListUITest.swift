cbc96738fc55dc73aafc045708e59d7fc747ac6f
Yoshifumi Sato <sato_yos@nifty.com>
2020-06-27 22:44:12 +0900

    Add UI tests for detail help screens

--- Shuffle100UITests/UITestWIthMultiScreen/HelpListUITest.swift
+++ Shuffle100UITests/UITestWIthMultiScreen/HelpListUITest.swift
@@ -19,10 +19,38 @@ class HelpListUITest: XCTestCase {
     }
 
     func test_gotoHelpListScreen() throws {
+       gotoHelpListScreen()
+    }
+    
+    func test_showAllDetailHelp() {
+        // given
+        gotoHelpListScreen()
+        // when, then
+        canGotoDetailHelp(title: "設定できること")
+        canGotoDetailHelp(title: "試合の流れ (通常モード)")
+        canGotoDetailHelp(title: "「初心者モード」とは？")
+        canGotoDetailHelp(title: "試合の流れ (初心者モード)")
+        canGotoDetailHelp(title: "「ノンストップ・モード」とは？")
+        canGotoDetailHelp(title: "「札セット」とその使い方")
+        canGotoDetailHelp(title: "「いなばくん」について")
+        
+    }
+
+    private func gotoHelpListScreen() {
         // when
         app.buttons["HelpButton"].tap()
         // then
         XCTAssert(app.navigationBars["ヘルプ"].exists)
     }
-
+    
+    private func canGotoDetailHelp(title: String) {
+        // when
+        app.tables.staticTexts[title].tap()
+        // then
+        XCTAssert(app.navigationBars[title].exists)
+        // when
+        app.navigationBars.buttons["ヘルプ"].tap()
+        // then
+        XCTAssert(app.navigationBars["ヘルプ"].exists)
+    }
 }
