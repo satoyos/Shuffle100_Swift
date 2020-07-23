8f43893c06821b84574896abe9b201b62801b220
Yoshifumi Sato <sato_yos@nifty.com>
2020-07-23 20:58:42 +0900

    Just introduce SaveSettings protocol

---
+++ Shuffle100/coordinaters/SaveSettingsProtocol.swift
@@ -0,0 +1,25 @@
+//
+//  SaveSettingsProtocol.swift
+//  Shuffle100
+//
+//  Created by Yoshifumi Sato on 2020/07/23.
+//  Copyright © 2020 里 佳史. All rights reserved.
+//
+
+import Foundation
+
+protocol SaveSettings {
+    var settings: Settings? { get set }
+    var store: StoreManager? { get set }
+    func saveSettingsPermanently(_ settings: Settings, into store: StoreManager) -> Void
+}
+
+extension SaveSettings {
+    func saveSettingsPermanently(_ settings: Settings, into store: StoreManager) {
+        do {
+            try store.save(value: settings, key: Settings.userDefaultKey)
+        } catch {
+            assertionFailure("SettingsデータのUserDefautへの保存に失敗しました。")
+        }
+    }
+}
