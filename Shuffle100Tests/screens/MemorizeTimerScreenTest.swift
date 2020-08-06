8d665766a3985aaf42e9eafd6c9d313f2625dc20
Yoshifumi Sato <sato_yos@nifty.com>
2020-08-06 21:46:36 +0900

    Just introduced MemorizeTImerScreen

---
+++ Shuffle100Tests/screens/MemorizeTimerScreenTest.swift
@@ -0,0 +1,26 @@
+//
+//  MemorizeTimerScreenTest.swift
+//  Shuffle100Tests
+//
+//  Created by Yoshifumi Sato on 2020/08/06.
+//  Copyright © 2020 里 佳史. All rights reserved.
+//
+
+import XCTest
+@testable import Shuffle100
+
+class MemorizeTimerScreenTest: XCTestCase {
+    var screen = MemorizeTimerViewController()
+
+    override func setUpWithError() throws {
+        screen.loadViewIfNeeded()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func test_initialLayout() throws {
+        XCTAssertEqual(screen.title, "暗記時間タイマー")
+    }
+}
