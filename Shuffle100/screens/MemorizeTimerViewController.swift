af1135781e239b480900f3880ee60f295b8bab33
Yoshifumi Sato <sato_yos@nifty.com>
2020-08-06 21:46:36 +0900

    Just introduced MemorizeTImerScreen

---
+++ Shuffle100/screens/MemorizeTimerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MemorizeTimerViewController.swift
+//  Shuffle100
+//
+//  Created by Yoshifumi Sato on 2020/08/06.
+//  Copyright © 2020 里 佳史. All rights reserved.
+//
+
+import UIKit
+
+class MemorizeTimerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "暗記時間タイマー"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
