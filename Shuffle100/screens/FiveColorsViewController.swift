02ef7d7a2d6a39a73617f3da9fe95815ac3dd082
Yoshifumi Sato <sato_yos@nifty.com>
2020-09-10 22:49:08 +0900

    Just introduced FiveColorsScreen (FiveColorsViewController)

---
+++ Shuffle100/screens/FiveColorsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FiveColorsViewController.swift
+//  Shuffle100
+//
+//  Created by Yoshifumi Sato on 2020/09/10.
+//  Copyright © 2020 里 佳史. All rights reserved.
+//
+
+import UIKit
+
+class FiveColorsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "五色百人一首"
+    }
+    
+
+}
