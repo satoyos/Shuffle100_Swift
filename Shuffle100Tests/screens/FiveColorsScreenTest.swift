6f4f2c4393c0a2ba0933fc405c62d8f30a1d34c7
Yoshifumi Sato <sato_yos@nifty.com>
2020-09-10 22:49:08 +0900

    Just introduced FiveColorsScreen (FiveColorsViewController)

---
+++ Shuffle100Tests/screens/FiveColorsScreenTest.swift
@@ -0,0 +1,34 @@
+//
+//  FiveColorsScreenTest.swift
+//  Shuffle100Tests
+//
+//  Created by Yoshifumi Sato on 2020/09/10.
+//  Copyright © 2020 里 佳史. All rights reserved.
+//
+
+import XCTest
+@testable import Shuffle100
+
+class FiveColorsScreenTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func test_initialScreen() throws {
+        // given, when
+        let screen = FiveColorsViewController()
+        // then
+        XCTAssertNotNil(screen)
+        // when
+        screen.loadViewIfNeeded()
+        // then
+        XCTAssertEqual(screen.title, "五色百人一首")
+        
+    }
+
+}
