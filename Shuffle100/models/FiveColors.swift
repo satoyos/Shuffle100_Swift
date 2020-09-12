ffc21285331d03e221ab32215200df9a3c3ad4e1
Yoshifumi Sato <sato_yos@nifty.com>
2020-09-12 18:13:37 +0900

    Set all 5 colors data: image path is not set yet

--- Shuffle100/models/FiveColors.swift
+++ Shuffle100/models/FiveColors.swift
@@ -22,13 +22,18 @@ struct FiveColorData {
     let name: String
 }
 
+fileprivate let blueSetNumbers =   [ 3,  5,  6, 12, 14, 24, 30, 31, 50, 57, 61, 62, 69, 70, 74, 75, 76, 82, 91, 100]
+fileprivate let yellowSetNumbers = [ 2,  7, 10, 18, 32, 33, 37, 39, 46, 47, 55, 60, 78, 79, 81, 85, 87, 89, 94, 96]
+fileprivate let greenSetNumbers =  [ 8,  9, 11, 15, 17, 20, 23, 26, 29, 35, 36, 38, 41, 42, 54, 59, 68, 71, 92, 93]
+fileprivate let pinkSetNumbers =   [ 1,  4, 13, 16, 22, 28, 34, 40, 48, 51, 58, 65, 66, 72, 73, 80, 83, 84, 86, 97]
+fileprivate let orangeSetNumbers = [19, 21, 25, 27, 43, 44, 45, 49, 52, 53, 56, 63, 64, 67, 77, 88, 90, 95, 98, 99]
+
 struct FiveColorsDataHolder {
     static let sharedDic: Dictionary<FiveColors,FiveColorData> = [
-        .blue: FiveColorData(type: .blue,
-                             poemNumbers: [ 3,  5,  6, 12, 14, 24, 30, 31, 50, 57, 61, 62, 69, 70, 74, 75, 76, 82, 91, 100],
-                             name: "青"),
-        .yellow: FiveColorData(type: .yellow,
-                               poemNumbers: [ 2,  7, 10, 18, 32, 33, 37, 39, 46, 47, 55, 60, 78, 79, 81, 85, 87, 89, 94, 96],
-                               name: "黄")
+        .blue: FiveColorData(type: .blue, poemNumbers: blueSetNumbers, name: "青"),
+        .yellow: FiveColorData(type: .yellow, poemNumbers: yellowSetNumbers, name: "黄"),
+        .green: FiveColorData(type: .green, poemNumbers: greenSetNumbers, name: "緑"),
+        .pink: FiveColorData(type: .pink, poemNumbers: pinkSetNumbers, name: "桃(ピンク)"),
+        .orange: FiveColorData(type: .orange, poemNumbers: orangeSetNumbers, name: "橙(オレンジ)")
     ]
 }
