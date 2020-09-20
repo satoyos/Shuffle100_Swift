29f66b44923bb80a24afc374a23546ef241ff9a1
Yoshifumi Sato <sato_yos@nifty.com>
2020-09-20 21:01:59 +0900

    Hide "五色百人一首" until version 6.2

--- Shuffle100UITests/UITestWIthMultiScreen/FiveColorsPickerUITest.swift
+++ Shuffle100UITests/UITestWIthMultiScreen/FiveColorsPickerUITest.swift
@@ -30,7 +30,12 @@ class FiveColorsPickerUITest: XCTestCase, HomeScreenUITestUtils, PoemPickerScree
         button.tap()
         // then
         XCTAssert(app.buttons["1字目で選ぶ"].exists)
-        XCTAssert(app.buttons["五色百人一首の色で選ぶ"].exists)
+        
+        // 五色百人一首機能の追加は、Version 6.2になる予定。
+        //        XCTAssert(app.buttons["五色百人一首の色で選ぶ"].exists)
+        XCTContext.runActivity(named: "Version 6.1.1(iOS14の動作確認リリース)では、五色百人一首機能はまだ隠す") { _ in
+            XCTAssertFalse(app.buttons["五色百人一首の色で選ぶ"].exists)
+        }
     }
 
 }
