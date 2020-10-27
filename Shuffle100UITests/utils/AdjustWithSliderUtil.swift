b7cab95ce24d16d35e27025ae927e73c34a021a4
Yoshifumi Sato <sato_yos@nifty.com>
2020-10-27 22:05:01 +0900

    Fix missing values when enlarging chewing value range

--- Shuffle100UITests/utils/AdjustWithSliderUtil.swift
+++ Shuffle100UITests/utils/AdjustWithSliderUtil.swift
@@ -15,10 +15,16 @@ protocol AdjustWithSliderUtils {
 extension AdjustWithSliderUtils {
     func staticDigitTextExistAround(_ digitValue: CGFloat, in app: XCUIApplication) {
         let equalStr =   String(format: "%.2f", digitValue)
-        let smallerStr = String(format: "%.2f", digitValue - 0.02)
-        let largerStr  = String(format: "%.2f", digitValue + 0.02)
+        let smallerStr = String(format: "%.2f", digitValue - 0.01)
+        let muchSmallerStr = String(format: "%.2f", digitValue - 0.02)
+        let largerStr  = String(format: "%.2f", digitValue + 0.01)
+        let muchLargerStr = String(format: "%.2f", digitValue + 0.02)
         // then
-        XCTAssert(app.staticTexts[smallerStr].exists || app.staticTexts[equalStr].exists ||
-                    app.staticTexts[largerStr].exists)
+        XCTAssert(
+            app.staticTexts[smallerStr].exists || app.staticTexts[equalStr].exists ||
+                    app.staticTexts[largerStr].exists ||
+                app.staticTexts[muchSmallerStr].exists ||
+                app.staticTexts[muchLargerStr].exists
+        )
     }
 }
