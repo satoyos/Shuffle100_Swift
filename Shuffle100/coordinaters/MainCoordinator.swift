552a1e8184dfba0e6d840de7a83dcb6ae51ecba8
Yoshifumi Sato <sato_yos@nifty.com>
2020-11-12 23:14:51 +0900

    Refactor: reduce needless "guard let" in MainCoordinator as much as possible

--- Shuffle100/coordinaters/MainCoordinator.swift
+++ Shuffle100/coordinaters/MainCoordinator.swift
@@ -47,51 +47,47 @@ class MainCoordinator: Coordinator, SaveSettings, HandleNavigator {
         homeScreen.selectPoemAction = {[weak self, unowned settings, store, unowned navigator] in
             self?.selectPoem(settings: settings, store: store, navigator: navigator)
         }
-        homeScreen.selectModeAction = {[weak self, unowned settings, store] in
-            self?.selectMode(settings: settings, store: store)
+        homeScreen.selectModeAction = {[weak self, unowned settings, store, unowned navigator] in
+            self?.selectMode(settings: settings, store: store, navigator: navigator)
         }
-        homeScreen.selectSingerAction = {[weak self, unowned settings, store] in
-            self?.selectSinger(settings: settings, store: store)
+        homeScreen.selectSingerAction = {[weak self, unowned settings, store, unowned navigator] in
+            self?.selectSinger(settings: settings, store: store, navigator: navigator)
         }
-        homeScreen.startGameAction = {[weak self, unowned settings, store] in
-            self?.startGame(settings: settings, store: store)
+        homeScreen.startGameAction = {[weak self, unowned settings, store, unowned navigator] in
+            self?.startGame(settings: settings, store: store, navigator: navigator)
         }
         homeScreen.reciteSettingsAction = { [weak self, unowned settings, store] in
-            self?.openReciteSettings(settins: settings, store: store)
+            self?.openReciteSettings(from: homeScreen, settins: settings, store: store)
         }
-        homeScreen.helpActioh = { [weak self] in
-            self?.openHelpList()
+        homeScreen.helpActioh = { [weak self, unowned navigator] in
+            self?.openHelpList(navigator: navigator)
         }
-        homeScreen.memorizeTimerAction = { [weak self] in
-            self?.openMemorizeTimer()
+        homeScreen.memorizeTimerAction = { [weak self, unowned navigator] in
+            self?.openMemorizeTimer(navigator: navigator)
         }
         setSaveSettingsActionTo(screen: homeScreen, settings: settings, store: store)
     }
         
     private func selectPoem(settings: Settings, store: StoreManager, navigator: UINavigationController) {
-//        guard let navigator = navigator else { return }
         let coordinator = PoemPickerCoordinator(navigator: navigator, settings: settings, store: store)
         coordinator.start()
         self.poemPickerCoordinator = coordinator
     }
     
-    private func selectMode(settings: Settings, store: StoreManager) {
-        guard let navigator = navigator else { return }
+    private func selectMode(settings: Settings, store: StoreManager, navigator: UINavigationController) {
         let coordinator = SelectModeCoordinator(navigator: navigator, settings: settings, store: store)
         coordinator.start()
         
     }
     
-    private func selectSinger(settings: Settings, store: StoreManager) {
-        guard let navigator = navigator else { return }
+    private func selectSinger(settings: Settings, store: StoreManager, navigator: UINavigationController) {
         let coordinator = SelectSingerCoordinator(navigator: navigator, settings: settings, store: store)
         coordinator.start()
     }
 
-    private func startGame(settings: Settings, store: StoreManager) {
+    private func startGame(settings: Settings, store: StoreManager, navigator: UINavigationController) {
         var gameDriver: RecitePoemCoordinator!
         
-        guard let navigator = navigator else { return }
         switch settings.mode.reciteMode {
         case .normal:
             gameDriver = NormalModeCoordinator(navigator: navigator, settings: settings, store: store)
@@ -113,24 +109,19 @@ class MainCoordinator: Coordinator, SaveSettings, HandleNavigator {
         }
     }
     
-    private func openReciteSettings(settins: Settings, store: StoreManager) {
-        guard let homeScreen = self.screen else { return }
-        let coordinator = ReciteSettingsCoordinator(settings: settins, fromScreen: homeScreen, store: store)
+    private func openReciteSettings(from screen: UIViewController, settins: Settings, store: StoreManager) {
+        let coordinator = ReciteSettingsCoordinator(settings: settins, fromScreen: screen, store: store)
         coordinator.start()
         self.reciteSettingsCoordinator = coordinator
     }
     
-    private func openHelpList() {
-        guard let navigator = navigator else { return }
-
+    private func openHelpList(navigator: UINavigationController) {
         let coordinator = HelpListCoordinator(navigator: navigator)
         coordinator.start()
         self.helpListCoordinator = coordinator
     }
     
-    private func openMemorizeTimer() {
-        guard let navigator = navigator else { return }
-
+    private func openMemorizeTimer(navigator: UINavigationController) {
         let coordinator = MemorizeTimerCoordinator(navigator: navigator)
         coordinator.start()
         self.memorizeTimerCoordinator = coordinator
