8359859b044d117dfb5e1ae7d05e09f1551069aa
Yoshifumi Sato <sato_yos@nifty.com>
2020-11-15 23:06:51 +0900

    Fix mistaking screenshots error

--- appium_tests/application_drivers/screenshots.rb
+++ appium_tests/application_drivers/screenshots.rb
@@ -4,6 +4,7 @@ require_relative 'string_with_utf8_mac'
 SCREEN_SHOTS_FOLDER = './screenshots'
 
 def take_screenshot_no(num)
+  sleep_while_animation
   @driver.save_screenshot("#{SCREEN_SHOTS_FOLDER}/#{@device_name.gsub(/ /, '_')}_#{num}.png")
 end
 
