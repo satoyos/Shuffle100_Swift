ffdf6dd2e5499ccd558e7e2d0ff4886805d4f1d1
Yoshifumi Sato <sato_yos@nifty.com>
2020-12-15 22:16:27 +0900

    Trial to reduce memory leak: MainCoordinator has just one child coordinator

--- Shuffle100/coordinaters/MainCoordinator.swift
+++ Shuffle100/coordinaters/MainCoordinator.swift
@@ -9,12 +9,14 @@
 import UIKit
 
 class MainCoordinator: NSObject, Coordinator, SaveSettings, HandleNavigator, UINavigationControllerDelegate {
+
     internal var settings: Settings
     internal var store: StoreManager
     internal var screen: UIViewController?
     var navigationController: UINavigationController
     static let env = Environment()
     var childCoordinators = [Coordinator]()
+    var childCoordinator: Coordinator?
 
     init(navigationController: UINavigationController){
         self.navigationController = navigationController
@@ -33,32 +35,32 @@ class MainCoordinator: NSObject, Coordinator, SaveSettings, HandleNavigator, UIN
         AudioPlayerFactory.shared.setupAudioSession()
     }
     
-    func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
-        // Read the view controller we’re moving from.
-        guard let fromViewController = navigationController.transitionCoordinator?.viewController(forKey: .from) else {
-            return
-        }
-
-        // Check whether our view controller array already contains that view controller. If it does it means we’re pushing a different view controller on top rather than popping it, so exit.
-        if navigationController.viewControllers.contains(fromViewController) {
-            return
-        }
-
-        // We’re still here – it means we’re popping the view controller, so we can check whether it’s a buy view controller
-        // We're popping a buy view controller; end its coordinator
-        childDidFinishOf(screen: fromViewController)
-    }
-    
-    func childDidFinishOf(screen childScreen: UIViewController) {
-        print("childDidFinishedOf()には来たよ！(子供の数: \(childCoordinators.count)")
-        for (index, coordinator) in childCoordinators.enumerated() {
-            if coordinator.screen === childScreen {
-                childCoordinators.remove(at: index)
-                print("No.\(index): \(coordinator)を削除しました。")
-                break
-            }
-        }
-    }
+//    func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
+//        // Read the view controller we’re moving from.
+//        guard let fromViewController = navigationController.transitionCoordinator?.viewController(forKey: .from) else {
+//            return
+//        }
+//
+//        // Check whether our view controller array already contains that view controller. If it does it means we’re pushing a different view controller on top rather than popping it, so exit.
+//        if navigationController.viewControllers.contains(fromViewController) {
+//            return
+//        }
+//
+//        // We’re still here – it means we’re popping the view controller, so we can check whether it’s a buy view controller
+//        // We're popping a buy view controller; end its coordinator
+//        childDidFinishOf(screen: fromViewController)
+//    }
+//
+//    func childDidFinishOf(screen childScreen: UIViewController) {
+//        print("childDidFinishedOf()には来たよ！(子供の数: \(childCoordinators.count)")
+//        for (index, coordinator) in childCoordinators.enumerated() {
+//            if coordinator.screen === childScreen {
+//                childCoordinators.remove(at: index)
+//                print("No.\(index): \(coordinator)を削除しました。")
+//                break
+//            }
+//        }
+//    }
 
     private func setActions(in homeScreen: HomeViewController, settings: Settings, store: StoreManager, navigator: UINavigationController) {
         homeScreen.selectPoemAction = {[weak self, unowned settings, store] in
@@ -88,19 +90,22 @@ class MainCoordinator: NSObject, Coordinator, SaveSettings, HandleNavigator, UIN
     private func selectPoem(settings: Settings, store: StoreManager) {
         let coordinator = PoemPickerCoordinator(navigationController: navigationController, settings: settings, store: store)
         coordinator.start()
-        childCoordinators.append(coordinator)
+//        childCoordinators.append(coordinator)
+        self.childCoordinator = coordinator
     }
 
     private func selectMode(settings: Settings, store: StoreManager) {
         let coordinator = SelectModeCoordinator(navigationController: navigationController, settings: settings, store: store)
         coordinator.start()
-        childCoordinators.append(coordinator)
+//        childCoordinators.append(coordinator)
+        self.childCoordinator = coordinator
     }
 
     private func selectSinger(settings: Settings, store: StoreManager) {
         let coordinator = SelectSingerCoordinator(navigationController: navigationController, settings: settings, store: store)
         coordinator.start()
-        childCoordinators.append(coordinator)
+//        childCoordinators.append(coordinator)
+        self.childCoordinator = coordinator
     }
 
     private func startGame(settings: Settings, store: StoreManager) {
@@ -118,7 +123,8 @@ class MainCoordinator: NSObject, Coordinator, SaveSettings, HandleNavigator, UIN
             gameDriver.start()
         }
         guard let coordinator = gameDriver else { return }
-        childCoordinators.append(coordinator)
+//        childCoordinators.append(coordinator)
+        self.childCoordinator = coordinator
     }
 
     private func setSaveSettingsActionTo(screen: HomeViewController, settings: Settings, store: StoreManager) {
@@ -130,18 +136,21 @@ class MainCoordinator: NSObject, Coordinator, SaveSettings, HandleNavigator, UIN
     private func openReciteSettings(from screen: UIViewController, settins: Settings, store: StoreManager) {
         let coordinator = ReciteSettingsCoordinator(settings: settins, fromScreen: screen, store: store)
         coordinator.start()
-        childCoordinators.append(coordinator)
+//        childCoordinators.append(coordinator)
+        self.childCoordinator = coordinator
     }
 
     private func openHelpList() {
         let coordinator = HelpListCoordinator(navigationController: navigationController)
         coordinator.start()
-        childCoordinators.append(coordinator)
+//        childCoordinators.append(coordinator)
+        self.childCoordinator = coordinator
     }
 
     private func openMemorizeTimer() {
         let coordinator = MemorizeTimerCoordinator(navigationController: navigationController)
         coordinator.start()
-        childCoordinators.append(coordinator)
+        self.childCoordinator = coordinator
+//        childCoordinators.append(coordinator)
     }
 }
