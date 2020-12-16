40e897e41ae62c7c99f77f3e189352eb80ae1a17
Yoshifumi Sato <sato_yos@nifty.com>
2020-12-16 19:19:03 +0900

    Delete uncertain code:
      Originally according to "Advanced coordinators in iOS!, https://www.hackingwithswift.com/articles/175/advanced-coordinator-pattern-tutorial-ios

--- Shuffle100/coordinaters/MainCoordinator.swift
+++ Shuffle100/coordinaters/MainCoordinator.swift
@@ -33,29 +33,6 @@ class MainCoordinator: NSObject, Coordinator, SaveSettings, HandleNavigator, UIN
         setActions(in: homeScreen, settings: settings, store: store, navigator: navigationController)
         AudioPlayerFactory.shared.setupAudioSession()
     }
-    
-    func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
-        // Read the view controller we’re moving from.
-        guard let fromViewController = navigationController.transitionCoordinator?.viewController(forKey: .from) else {
-            return
-        }
-
-        // Check whether our view controller array already contains that view controller. If it does it means we’re pushing a different view controller on top rather than popping it, so exit.
-        if navigationController.viewControllers.contains(fromViewController) {
-            return
-        }
-
-        // We’re still here – it means we’re popping the view controller, so we can check whether it’s a buy view controller
-        // We're popping a buy view controller; end its coordinator
-        childDidFinish()
-    }
-
-    func childDidFinish() {
-        // If you want to know original concept of this mechanism,
-        // read article "Advanced coordinators in iOS" by Paul Hudson,
-        // https://www.hackingwithswift.com/articles/175/advanced-coordinator-pattern-tutorial-ios
-        self.childCoordinator = nil
-    }
 
     private func setActions(in homeScreen: HomeViewController, settings: Settings, store: StoreManager, navigator: UINavigationController) {
         homeScreen.selectPoemAction = {[weak self, unowned settings, store] in
