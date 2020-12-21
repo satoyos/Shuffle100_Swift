baec6a56b60dc6b1cc8cf77d2b94192023b1e759
Yoshifumi Sato <sato_yos@nifty.com>
2020-12-21 19:02:16 +0900

    Rename MemorizeTimerViewController to MemorizeTimerScreen

--- Shuffle100/Layout/MemorizeTimerScreenLayout.swift
+++ Shuffle100/Layout/MemorizeTimerScreenLayout.swift
@@ -10,7 +10,7 @@ import UIKit
 import SnapKit
 import Then
 
-extension MemorizeTimerViewController {
+extension MemorizeTimerScreen {
     internal func layoutScreen() {
         configureTimerContainer()
         configurePlayButton()
