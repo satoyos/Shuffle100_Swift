fc440ac6bf8c97ba3225090ee5c89530293854bf
Yoshifumi Sato <sato_yos@nifty.com>
2020-12-21 22:10:19 +0900

    Fix bug: couldn't go next poem in Beginner Mode

--- Shuffle100/coordinaters/RecitePoemCoordinator.swift
+++ Shuffle100/coordinaters/RecitePoemCoordinator.swift
@@ -35,9 +35,6 @@ class RecitePoemCoordinator: Coordinator{
         screen.openSettingsAction = { [weak self] in
             self?.openReciteSettings()
         }
-        screen.viewDidAppearAction = { [weak self] in
-            self?.childCoordinator = nil
-        }
         // 序歌の読み上げは画面遷移が完了したタイミングで開始したいので、
         // CATransanctionを使って、遷移アニメーション完了コールバックを使う。
         CATransaction.begin()
