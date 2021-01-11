fdcc13c76764f49aa0465391920303e0a6cdd31c
Yoshifumi Sato <sato_yos@nifty.com>
2021-01-11 19:41:25 +0900

    Modify how to configure each cell in ReciteSettingsScreen
     Introduce UIListContentConfiguration, instead of  deprecated "textLabel" and "detailTextLabel"

---
+++ Shuffle100Tests/views/ReciteSettingsTableCellTest.swift
@@ -0,0 +1,31 @@
+//
+//  ReciteSettingsTableCellTest.swift
+//  Shuffle100Tests
+//
+//  Created by Yoshifumi Sato on 2021/01/11.
+//  Copyright © 2021 里 佳史. All rights reserved.
+//
+
+import XCTest
+
+class ReciteSettingsTableCellTest: XCTestCase {
+    let screen = ReciteSettingsScreen()
+
+    override func setUpWithError() throws {
+        screen.loadViewIfNeeded()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func test_firstCellConfigurationIsSet() throws {
+        let firstCell = screen.tableView(screen.tableView, cellForRowAt: IndexPath(row: 0, section: 0))
+        let cellCoonfig = firstCell.contentConfiguration as? UIListContentConfiguration ?? nil
+        XCTAssertNotNil(cellCoonfig)
+        XCTAssertEqual(cellCoonfig?.text, "歌と歌の間隔")
+        XCTAssertEqual(cellCoonfig?.secondaryText, "1.10")
+    }
+
+
+}
