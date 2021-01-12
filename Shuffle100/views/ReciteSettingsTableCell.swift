e6f9d245436dabfb9c6857f7393810bc69916910
Yoshifumi Sato <sato_yos@nifty.com>
2021-01-12 21:27:32 +0900

    Delete unneeded initializer for HomeScreenTableCell and ReciteSettingsTableCell

--- Shuffle100/views/ReciteSettingsTableCell.swift
+++ Shuffle100/views/ReciteSettingsTableCell.swift
@@ -10,14 +10,14 @@ import UIKit
 
 class ReciteSettingsTableCell: UITableViewCell, ConfigureTableCell {
     static let identifier = "ReciteSettingsTableCell"
-    var cellStyle: UITableViewCell.CellStyle
-    
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        cellStyle = .value1
-        super.init(style: cellStyle, reuseIdentifier: reuseIdentifier)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+//    var cellStyle: UITableViewCell.CellStyle
+//    
+//    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+//        cellStyle = .value1
+//        super.init(style: cellStyle, reuseIdentifier: reuseIdentifier)
+//    }
+//    
+//    required init?(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
 }
