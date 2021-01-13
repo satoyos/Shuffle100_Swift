b8fb39d1269eb5fc0c3ca5f36d52661a84b49b28
Yoshifumi Sato <sato_yos@nifty.com>
2021-01-13 20:14:30 +0900

    Just Introduce SettingTableCell class

---
+++ Shuffle100/views/SettingTableCell.swift
@@ -0,0 +1,13 @@
+//
+//  SettingTableCell.swift
+//  Shuffle100
+//
+//  Created by Yoshifumi Sato on 2021/01/13.
+//  Copyright © 2021 里 佳史. All rights reserved.
+//
+
+import UIKit
+
+class SettingTableCell: UITableViewCell, ConfigureSettingTableCell {
+
+}
