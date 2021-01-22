f9426eb9d108dd710ceb8479ff6b197a0313ed12
Yoshifumi Sato <sato_yos@nifty.com>
2021-01-22 22:43:28 +0900

    Comment out all code in FudaSetTableViewCell to prepare removing the class	th

--- Shuffle100/FudaSetsTableViewCell.swift
+++ Shuffle100/FudaSetsTableViewCell.swift
@@ -8,15 +8,19 @@
 
 import UIKit
 
+//
+// このクラス、消してしまっていいのでは？
+//
+
 class FudaSetsTableViewCell: UITableViewCell {
-    var cellStyle: UITableViewCell.CellStyle
-    
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        cellStyle = .value1
-        super.init(style: cellStyle, reuseIdentifier: reuseIdentifier)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+//    var cellStyle: UITableViewCell.CellStyle
+//    
+//    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+//        cellStyle = .value1
+//        super.init(style: cellStyle, reuseIdentifier: reuseIdentifier)
+//    }
+//    
+//    required init?(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
 }
