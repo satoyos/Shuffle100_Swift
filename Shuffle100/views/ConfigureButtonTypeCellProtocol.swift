d80789481aa43bd562a7f9e00ba323812d24e071
Yoshifumi Sato <sato_yos@nifty.com>
2021-01-23 18:31:41 +0900

    Refactor: cut out protocol for ButtonTypeCell from GameStartCell.swift

---
+++ Shuffle100/views/ConfigureButtonTypeCellProtocol.swift
@@ -0,0 +1,52 @@
+//
+//  ConfigureButtonTypeCellProtocol.swift
+//  Shuffle100
+//
+//  Created by Yoshifumi Sato on 2021/01/23.
+//  Copyright © 2021 里 佳史. All rights reserved.
+//
+
+import UIKit
+
+protocol ConfigureButtonTypeCell {
+    func configure(dataSource: ButtonTypeCellDataSource)
+}
+
+extension ConfigureButtonTypeCell where Self: UITableViewCell {
+    func configure(dataSource: ButtonTypeCellDataSource) {
+        
+        self.contentConfiguration = createListContentConfiguration(from: dataSource)
+        self.accessibilityLabel = dataSource.accessibilityLabel
+    }
+    
+    private func createListContentConfiguration(from dataSource: ButtonTypeCellDataSource) -> UIListContentConfiguration {
+        var content: UIListContentConfiguration = .cell()
+        content.text = dataSource.title
+        content.textProperties.color = colorFromDataSource(dataSource)
+        content.textProperties.font = UIFont.systemFont(ofSize: UIFont.labelFontSize,                                          weight: fontWeightFromDataSource(dataSource))
+        content.textProperties.alignment = .center
+        return content
+    }
+    
+    private func colorFromDataSource(_ dataSource: ButtonTypeCellDataSource) -> UIColor {
+        let color: UIColor
+        switch dataSource.titleColor {
+        case .red:
+            color = .systemRed
+        default:
+            color = .label
+        }
+        return color
+    }
+    
+    private func fontWeightFromDataSource(_ dataSource: ButtonTypeCellDataSource) -> UIFont.Weight {
+        let weight: UIFont.Weight
+        switch dataSource.fontWeight {
+        case .bold:
+            weight = .bold
+        default:
+            weight = .regular
+        }
+        return weight
+    }
+}
--- Shuffle100/views/GameStartCell.swift
+++ Shuffle100/views/GameStartCell.swift
@@ -8,47 +8,4 @@
 
 import UIKit
 
-protocol ConfigureButtonTypeCell {
-    func configure(dataSource: ButtonTypeCellDataSource)
-}
-
-extension ConfigureButtonTypeCell where Self: UITableViewCell {
-    func configure(dataSource: ButtonTypeCellDataSource) {
-        
-        self.contentConfiguration = createListContentConfiguration(from: dataSource)
-        self.accessibilityLabel = dataSource.accessibilityLabel
-    }
-    
-    private func createListContentConfiguration(from dataSource: ButtonTypeCellDataSource) -> UIListContentConfiguration {
-        var content: UIListContentConfiguration = .cell()
-        content.text = dataSource.title
-        content.textProperties.color = colorFromDataSource(dataSource)
-        content.textProperties.font = UIFont.systemFont(ofSize: UIFont.labelFontSize,                                          weight: fontWeightFromDataSource(dataSource))
-        content.textProperties.alignment = .center
-        return content
-    }
-    
-    private func colorFromDataSource(_ dataSource: ButtonTypeCellDataSource) -> UIColor {
-        let color: UIColor
-        switch dataSource.titleColor {
-        case .red:
-            color = .systemRed
-        default:
-            color = .label
-        }
-        return color
-    }
-    
-    private func fontWeightFromDataSource(_ dataSource: ButtonTypeCellDataSource) -> UIFont.Weight {
-        let weight: UIFont.Weight
-        switch dataSource.fontWeight {
-        case .bold:
-            weight = .bold
-        default:
-            weight = .regular
-        }
-        return weight
-    }
-}
-
 class GameStartCell: UITableViewCell, ConfigureButtonTypeCell { }
