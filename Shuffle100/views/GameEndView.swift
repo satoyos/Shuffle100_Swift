ec7e454f2752b42aad9c7b7ae48a19593148d35e
Yoshifumi Sato <sato_yos@nifty.com>
2021-03-01 21:04:49 +0900

    Refactor: remove unused code
    - already comment out

--- Shuffle100/views/GameEndView.swift
+++ Shuffle100/views/GameEndView.swift
@@ -38,7 +38,6 @@ extension AllPoemsRecitedView where Self: UIView {
         self.addSubview(backToHomeButton)
         layoutHeaderContainer()
         layoutBackToHomeButtonCenter()
-//        backToHomeButton.addTarget(self, action: #selector(backToHomeButtonTapped), for: .touchUpInside)
         setTargetToButtons()
     }
     
@@ -79,81 +78,16 @@ extension AllPoemsRecitedView where Self: UIView {
 
 class GameEndView: UIView, AllPoemsRecitedView {
     var backToHomeButton = UIButton()
-//    var postMortemButton: UIButton! = nil
     var headerContainer = UIView()
     var headerTitle: String?
     var backToHomeButtonAction: ( ()->Void )?
 
-//    func fixLayoutOn(baseView: UIView) {
-//        self.snp.remakeConstraints{(make) -> Void in
-//            make.top.equalTo(baseView.safeAreaInsets.top)
-//            make.bottom.equalToSuperview()
-//            make.width.equalToSuperview()
-//            make.left.equalToSuperview()
-//        }
-//        baseView.layoutSubviews()
-//    }
 
-////    func initView(title: String, postMortemEnabled: Bool = false) {
-//    func initView(title: String) {
-//        self.backgroundColor = UIColor.systemBackground
-//        self.headerTitle = title
-//        backToHomeButton.setTitleColor(StandardColor.standardButtonColor, for: .normal)
-//        self.addSubview(backToHomeButton)
-//        layoutHeaderContainer()
-////        if postMortemEnabled {
-////            self.postMortemButton = UIButton()
-////        } else {
-//            layoutBackToHomeButtonCenter()
-////        }
-//        backToHomeButton.addTarget(self, action: #selector(backToHomeButtonTapped), for: .touchUpInside)
-//    }
-    
-//    private func layoutBackToHomeButtonCenter() {
-//        backToHomeButton.setTitle("トップに戻る", for: .normal)
-//        backToHomeButton.sizeToFit()
-//        backToHomeButton.snp.makeConstraints{(make) -> Void in
-//            make.center.equalTo(self)
-//        }
-//    }
-    
-//    private func layoutHeaderContainer() {
-//        self.addSubview(headerContainer)
-//        headerContainer.backgroundColor = StandardColor.barTintColor
-//        headerContainer.snp.makeConstraints{(make) -> Void in
-//            make.width.equalToSuperview()
-//            make.height.equalTo(40)
-//            make.top.equalToSuperview()
-//            make.left.equalToSuperview()
-//        }
-//        layoutHeaderTitle()
-//    }
-    
-//    private func layoutHeaderTitle() {
-//        let titleLabel = UILabel().then {
-//            $0.text = headerTitle
-//            $0.textAlignment = .center
-//            $0.sizeToFit()
-//            $0.backgroundColor = .clear
-//            $0.textColor = UIColor.label
-//        }
-//        headerContainer.addSubview(titleLabel)
-//        titleLabel.snp.makeConstraints{(make) -> Void in
-//            make.center.equalToSuperview()
-//        }
-//    }
-    
     internal func setTargetToButtons() {
         backToHomeButton.addTarget(self, action: #selector(backToHomeButtonTapped), for: .touchUpInside)
     }
     
     @objc func backToHomeButtonTapped() {
-//        print("GameEndViewで定義されたdelegateメソッドが呼ばれた！")
-//        guard let action = backToHomeButtonAction else {
-//            print("No action is set to this button!")
-//            return
-//        }
-//        action()
         backToHomeButtonAction?()
     }
 }
