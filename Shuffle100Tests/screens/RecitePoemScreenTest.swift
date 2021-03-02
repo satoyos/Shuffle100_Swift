2419be06324fef722869f0cd8ff0b35d5dba989f
Yoshifumi Sato <sato_yos@nifty.com>
2021-03-02 22:57:06 +0900

    Fix caution in Unit Test

--- Shuffle100Tests/screens/RecitePoemScreenTest.swift
+++ Shuffle100Tests/screens/RecitePoemScreenTest.swift
@@ -70,7 +70,7 @@ class RecitePoemScreenTest: XCTestCase {
         // then
         let gameEndView = screen.gameEndView
         XCTAssertNotNil(gameEndView)
-        XCTAssert(gameEndView is PostMortemEnabledGameEndView)
+        XCTAssertFalse(gameEndView is SimpleGameEndView)
         XCTAssertNotNil(gameEndView?.backToHomeButton)
     }
 }
