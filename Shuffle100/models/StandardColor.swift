68b419ec269d6378cdaa59cebc9e157dc14b84bb
Yoshifumi Sato <sato_yos@nifty.com>
2021-03-05 21:14:52 +0900

    Modify standard button color

--- Shuffle100/models/StandardColor.swift
+++ Shuffle100/models/StandardColor.swift
@@ -11,7 +11,7 @@ import UIKit
 struct StandardColor {
     static let barTintColor = UIColor.dynamicColor(light: Color.natsumushi.UIColor, dark: Color.momoshiocha.UIColor)
     static let selectedPoemBackColor = UIColor.dynamicColor(light: Color.nadeshiko.UIColor, dark: Color.koikurenai.UIColor)
-    static let standardButtonColor = UIColor.dynamicColor(light: Color.konpeki.UIColor, dark: Color.aofujiiro.UIColor)
+    static let standardButtonColor = UIColor.dynamicColor(light: UIColor.systemIndigo, dark: UIColor.systemTeal)
     static let disabledButtonColor = UIColor.dynamicColor(light: .lightGray, dark: .darkGray)
     static let backgroundColor = standardBackgroundColor()
     
