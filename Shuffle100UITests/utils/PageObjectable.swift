691f7aab0e9d48aebc4862363ab5eef4f553b0de
Yoshifumi Sato <sato_yos@nifty.com>
2021-04-03 22:21:45 +0900

    Add PageObjectable protocol

---
+++ Shuffle100UITests/utils/PageObjectable.swift
@@ -0,0 +1,39 @@
+//
+//  PageObjectable.swift
+//  Shuffle100UITests
+//
+//  Created by Yoshifumi Sato on 2021/04/03.
+//  Copyright © 2021 里 佳史. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+protocol PageObjectable {
+    associatedtype Ally
+    var app: XCUIApplication { get }
+    init(app: XCUIApplication)
+    var exists: Bool { get }
+    var pageTitle: XCUIElement { get }
+    func elementsExist(_ elements: [XCUIElement], timeout: Double) -> Bool
+}
+
+extension PageObjectable {
+    var app: XCUIApplication {
+        return XCUIApplication()
+    }
+    
+    var exists: Bool {
+        return elementsExist([pageTitle], timeout: 5)
+    }
+    
+    func elementsExist(_ elements: [XCUIElement], timeout: Double) -> Bool {
+        for element in elements {
+            if !element.waitForExistence(timeout: timeout) {
+                return false
+            }
+        }
+        
+        return true
+    }
+}
