43462bb269f3f5a2aa7989a3986a4994c0065e06
Yoshifumi Sato <sato_yos@nifty.com>
2021-04-25 21:04:22 +0900

    Rewrite PoemPickerIntegrationUITest with PageObjects
     - the test class doesn't need to confirm Utility protocol for it

--- Shuffle100UITests/UITestWIthMultiScreen/PoemPickerIntegrationUITest.swift
+++ Shuffle100UITests/UITestWIthMultiScreen/PoemPickerIntegrationUITest.swift
@@ -8,8 +8,7 @@
 
 import XCTest
 
-//class PoemPickerIntegrationUITest: XCTestCase, HomeScreenUITestUtils, PoemPickerScreenUITestUtils {
-class PoemPickerIntegrationUITest: XCTestCase, PoemPickerScreenUITestUtils {
+final class PoemPickerIntegrationUITest: XCTestCase {
 
     var app = XCUIApplication()
     lazy var homePage = HomePage(app: app)
@@ -26,17 +25,13 @@ class PoemPickerIntegrationUITest: XCTestCase, PoemPickerScreenUITestUtils {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
-    
-    func test_HomeScreenReflectsSelectioninPoemPicker() {
-        
+    func test_HomeScreenReflectsSelectioninPoemPicker() {        
         XCTContext.runActivity(named: "デフォルトのトップ画面に「100首」と書かれたセルが存在する") { (activity) in
             XCTAssert(homePage.numberOfSelecttedPoems(is: 100))
         }
         let poemPickerPage = homePage.goToPoemPickerPage()
         XCTAssert(poemPickerPage.exists, "「歌を選ぶ」画面に到達")
-        XCTContext.runActivity(named: "プレースホルダ付きの検索窓がある") { (activity) in
-            XCTAssert(app.searchFields["歌を検索"].exists)
-        }
+        XCTAssert(poemPickerPage.searchField.exists, "プレースホルダ付きの検索窓がある")
         XCTContext.runActivity(named: "歌を3つタップして選択状態を解除し、トップ画面に戻ると、歌の数が97首になっている") { (activity) in
             //   tap poems #1,2, 4
             poemPickerPage
@@ -50,54 +45,56 @@ class PoemPickerIntegrationUITest: XCTestCase, PoemPickerScreenUITestUtils {
     }
     
     func test_searchPoem() {
-        // given
-//        gotoPoemPickerScreen(app)
-        XCTAssert(homePage.goToPoemPickerPage().exists)
+        // when
+        let poemPickerPage = homePage.goToPoemPickerPage()
+        
         XCTContext.runActivity(named: "検索窓に「あき」を入力すると、検索用データがそれにヒットする歌のみ表示される"){
             (activity) in
             // when
-            let searchField = app.searchFields.element
+            let searchField = poemPickerPage.searchField
             searchField.tap()
             searchField.typeText("あき")
             // then
-            XCTAssert(app.tables.cells["001"].exists)
-            XCTAssertFalse(app.tables.cells["002"].exists)
-            XCTAssert(app.tables.cells["005"].exists)
+            XCTAssert(poemPickerPage.cellOf(number: 1).exists)
+            XCTAssertFalse(poemPickerPage.cellOf(number: 2).exists)
+            XCTAssert(poemPickerPage.cellOf(number: 5).exists)
         }
         XCTContext.runActivity(named: "検索結果としてフィルタリングされた歌をタップしたら、選択状態を変えることができる") { (acitvity) in
             // when
-            app.tables.cells["001"].tap()
-            app.tables.cells["005"].tap()
-            app.buttons["キャンセル"].tap()
+            poemPickerPage
+                .tapCellof(number: 1)
+                .tapCellof(number: 5)
+                .cancelButton.tap()
             //   back to HomeScreen
-            goBackToHomeScreen(app)
+            poemPickerPage.backToTopButton.tap()
             // then
-            XCTAssertTrue(app.cells.staticTexts["98首"].exists)
+            XCTAssert(homePage.numberOfSelecttedPoems(is: 98))
         }
     }
     
     func test_alertAppearsWhenStartGameWithoutSelectedPoem() {
         // given
-//        gotoPoemPickerScreen(app)
-        XCTAssert(homePage.goToPoemPickerPage().exists, "「歌を選ぶ」画面に到達")
-        let button = waitToHittable(for: app.buttons["全て取消"], timeout: timeOutSec)
-        button.tap()
-        goBackToHomeScreen(app)
-        XCTAssert(app.staticTexts["0首"].exists)
+        let poemPickerPage = homePage.goToPoemPickerPage()
+        // when
+        poemPickerPage.allCancellButton.tap()
+        poemPickerPage.backToTopButton.tap()
+        // then
+        XCTAssert(homePage.numberOfSelecttedPoems(is: 0))
+
         XCTContext.runActivity(named: "歌を全く選ばないまま試合を開始しようとすると、アラートが表示される") { activity in
             // when
-            app.cells["GameStartCell"].tap()
+            homePage.gameStartCell.tap()
             // then
-            XCTAssert(app.staticTexts["詩を選びましょう"].exists)
+            XCTAssert(homePage.noPoemSelectedAlert.exists)
         }
         XCTContext.runActivity(named: "アラート画面で「戻る」ボタンを押すと、アラートは消える") { acivity in
             // when
-            app.buttons["戻る"].tap()
+            homePage.dismissAlert()
             // then
-            XCTAssertFalse(app.staticTexts["詩を選びましょう"].exists)
+            XCTAssertFalse(homePage.noPoemSelectedAlert.exists)
         }
         XCTContext.runActivity(named: "アラート画面が消えてトップ画面に戻ったとき、「試合開始」セルの選択は解除された状態になっている") { activity in
-            XCTAssertFalse(app.cells["GameStartCell"].isSelected)
+            XCTAssertFalse(homePage.gameStartCell.isSelected)
         }
     }
 }
--- Shuffle100UITests/pages/PoemPickerPage.swift
+++ Shuffle100UITests/pages/PoemPickerPage.swift
@@ -9,7 +9,7 @@
 import Foundation
 import XCTest
 
-final class PoemPickerPage: PageObjectable {
+final class PoemPickerPage: PageObjectable, WaitInUITest {
     let app: XCUIApplication
     
     init(app: XCUIApplication) {
@@ -24,9 +24,24 @@ final class PoemPickerPage: PageObjectable {
         return app.navigationBars.buttons[A11y.backToTop].firstMatch
     }
     
+    var searchField: XCUIElement {
+        return app.searchFields[A11y.searchFieldPlaceHolder].firstMatch
+    }
+    
+    var cancelButton: XCUIElement {
+        return app.buttons[A11y.cancel].firstMatch
+    }
+    
+    var allCancellButton: XCUIElement {
+        return waitToHittable(for: app.buttons[A11y.allCancel], timeout: timeOutSec)
+    }
+    
     enum A11y {
         static let title = "歌を選ぶ"
         static let backToTop = "トップ"
+        static let searchFieldPlaceHolder = "歌を検索"
+        static let cancel = "キャンセル"
+        static let allCancel = "全て取消"
     }
     
     @discardableResult
@@ -44,5 +59,10 @@ final class PoemPickerPage: PageObjectable {
         let homePage = HomePage(app: app)
         XCTAssert(homePage.exists, "トップ画面に戻った")
         return homePage
+    }
+    
+    func cellOf(number: Int) -> XCUIElement {
+        let label = String(format: "%03d", number)
+        return app.tables.cells[label]
     }
 }
