285ae897de8ffeeae70aad1cad68ab7cf9ede172
Yoshifumi Sato <sato_yos@nifty.com>
2021-06-16 20:56:04 +0900

    Grouped cells on RecitingSettingsScreen

--- Shuffle100/dataSources/ReciteSettingsScreenDataSource.swift
+++ Shuffle100/dataSources/ReciteSettingsScreenDataSource.swift
@@ -11,16 +11,23 @@ import UIKit
 extension ReciteSettingsScreen: UITableViewDataSource {
 
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return sections.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section].title
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tableSources.count
+        return sections[section].dataSources.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseID, for: indexPath) as! SettingTableCell        
-        let source = tableSources[indexPath.row]
+        guard let source = sections[indexPath.section].dataSources[indexPath.row] as? SettingsCellDataSource else {
+            assertionFailure("Couldn't get TableDataSource as SettingsCellDataSource")
+            return cell
+        }
         cell.configure(dataSource: source)
         if let switchView = cell.accessoryView as? UISwitch {
             switchView.addTarget(self, action: #selector(switchValueChanged) , for: .valueChanged)
