31e93b4fd2d813b16e6022898efefc7d307568c8
Yoshifumi Sato <sato_yos@nifty.com>
2021-06-30 20:25:44 +0900

    Complete replacing UI test code around beginner mode

--- Shuffle100UITests/pages/WhatsNextPage.swift
+++ Shuffle100UITests/pages/WhatsNextPage.swift
@@ -36,11 +36,15 @@ final class WhatsNextpage: PageObjectable {
         return app.buttons[A11y.torifuda].firstMatch
     }
     
+    var refrainButton: XCUIElement {
+        return app.buttons[A11y.refrain].firstMatch
+    }
     
     enum A11y {
         static let title = "次はどうする？"
         static let nextPoem = "goNext"
         static let torifuda = "torifuda"
+        static let refrain = "refrain"
         static let gear = "gear"
         static let exit = "exit"
     }
