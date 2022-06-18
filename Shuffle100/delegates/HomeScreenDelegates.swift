6260bf49e8a70f93505c5156a5f8c5896207bb5b
Yoshifumi Sato <sato_yos@nifty.com>
2022-06-18 20:47:31 +0900

    Refactor HomeScreenDelegates

--- Shuffle100/delegates/HomeScreenDelegates.swift
+++ Shuffle100/delegates/HomeScreenDelegates.swift
@@ -15,31 +15,38 @@ extension HomeScreen: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        let cell = self.tableView(tableView, cellForRowAt: indexPath)
-        
         if indexPath.section == 0 {
-            let cellsNum = self.tableView(tableView, numberOfRowsInSection: 0)
-            switch indexPath.row {
-            case 0:
-                selectPoemAction?()
-            case 1:
-                selectModeAction?()
-            case cellsNum-1:
-                selectSingerAction?()
-            default:
-                return
+            execSettingsActions(on: tableView, at: indexPath)
+        } else {
+            execGameActions(on: tableView, at: indexPath)
+        }
+    }
+    
+    private func execGameActions(on tableVIew: UITableView, at indexPath: IndexPath) {
+        let cell = self.tableView(tableView, cellForRowAt: indexPath)
+        if settings.state100.selectedNum > 0 {
+            if cell.accessibilityLabel == startGameReuseId {
+                startGameAction?()
+            } else {
+                memorizeTimerAction?()
             }
         } else {
-            if settings.state100.selectedNum > 0 {
-                if cell.accessibilityLabel == startGameReuseId {
-                    startGameAction?()
-                } else {
-                    memorizeTimerAction?()
-                }
-            } else {
-                tableView.deselectRow(at: indexPath, animated: true)
-                alertZeroPoems()
-            }
+            tableView.deselectRow(at: indexPath, animated: true)
+            alertZeroPoemsSelected()
+        }
+    }
+    
+    private func execSettingsActions(on tableView: UITableView, at indexPath: IndexPath) {
+        let cellsNum = self.tableView(tableView, numberOfRowsInSection: 0)
+        switch indexPath.row {
+        case 0:
+            selectPoemAction?()
+        case 1:
+            selectModeAction?()
+        case cellsNum-1:
+            selectSingerAction?()
+        default:
+            return
         }
     }
     
@@ -56,7 +63,7 @@ extension HomeScreen: UITableViewDelegate {
         self.helpActioh?()
     }
     
-    private func alertZeroPoems() {
+    private func alertZeroPoemsSelected() {
         let backAction = UIAlertAction(title: "戻る", style: .cancel)
         let ac = UIAlertController(title: "歌を選びましょう", message: "「取り札を用意する歌」で、試合に使う歌を選んでください", preferredStyle: .alert)
         ac.addAction(backAction)
