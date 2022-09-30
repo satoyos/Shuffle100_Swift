85092432449929018f0719aa7b339736e09dd3cb
Yoshifumi Sato <sato_yos@nifty.com>
2022-09-30 23:47:57 +0900

    Move some code for dataSources from ReciteSettingsScreen to its delegate

--- Shuffle100/dataSources/ReciteSettingsScreenDataSource.swift
+++ Shuffle100/dataSources/ReciteSettingsScreenDataSource.swift
@@ -35,3 +35,57 @@ extension ReciteSettingsScreen: UITableViewDataSource {
         return cell
     }
 }
+
+extension ReciteSettingsScreen {
+    internal func setUpTableSources() {
+        self.sections = [
+            createInervalSection(),
+            createVolumeSection(),
+            createModeSection()
+        ]
+    }
+
+//
+//    ToDo: 以下の関数は Computed Propertyにできる
+//
+    
+    private func createInervalSection() -> TableSection {
+        var intervalSection = TableSection(title: A11y.intervalSectionTitle)
+        intervalSection.dataSources = [
+            SettingsCellDataSource(title: A11y.intervalCellTitle, accessoryType: .disclosureIndicator, secondaryText: String(format: "%.2F", settings.interval)),
+            SettingsCellDataSource(title: A11y.kamiShimoIntervalCellTitle, accessoryType: .disclosureIndicator, secondaryText: String(format: "%.2F", settings.kamiShimoInterval))
+        ]
+        return intervalSection
+    }
+    
+    private func createVolumeSection() -> TableSection {
+        var volumeSection = TableSection(title: A11y.volumeSectionTitle)
+        volumeSection.dataSources = [
+            SettingsCellDataSource(title: A11y.volumeCellTitle, accessoryType: .disclosureIndicator, secondaryText: "\(Int(settings.volume * 100))" + "%"),
+        ]
+        return volumeSection
+    }
+    
+    private func createModeSection() -> TableSection {
+        var modeSection = TableSection(title: A11y.detailGameModeScttionTitle)
+        modeSection.dataSources = [
+            shortenJokaTableDataSource(),
+            postMotermTableDataSource()
+        ]
+        return modeSection
+    }
+    
+    private func postMotermTableDataSource() -> SettingsCellDataSource {
+        var dataSource =             SettingsCellDataSource(title: A11y.postMortemCellTitle, accessoryType: .none, secondaryText: "感想戦では、同じ歌を同じ順序で読み上げます。", configType: .subtitleCell())
+        dataSource.withSwitchOf = settings.postMortemEnabled
+        dataSource.accessibilityLabel = A11y.postMortemA11yLabel
+        return dataSource
+    }
+    
+    private func shortenJokaTableDataSource() -> SettingsCellDataSource {
+        var dataSource = SettingsCellDataSource(title: A11y.shortenJokaCellTitle, accessoryType: .none, secondaryText: "序歌は、下の句を1回だけ読み上げます。", configType: .subtitleCell())
+        dataSource.withSwitchOf = settings.shortenJoka
+        dataSource.accessibilityLabel = A11y.shortenJokaA11yLabel
+        return dataSource
+    }
+}
