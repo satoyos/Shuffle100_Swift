614b81b79b888e837fe2cd92f4ef0148028ac0fe
Yoshifumi Sato <sato_yos@nifty.com>
2022-10-14 21:19:11 +0900

    Just introduced SelectedPoemsNumber protocol
     for commonly used calculated variables

---
+++ Shuffle100/screens/SelectedPoemsNumberProtocol.swift
@@ -0,0 +1,33 @@
+//
+//  SelectedPoemsNumberProtocol.swift
+//  Shuffle100
+//
+//  Created by Yoshifumi Sato on 2022/10/14.
+//  Copyright © 2022 里 佳史. All rights reserved.
+//
+
+import UIKit
+
+protocol SelectedPoemsNumber {
+    var selected_num: Int { get }
+    var selectedNumBadgeItem: UIBarButtonItem { get }
+}
+
+extension SelectedPoemsNumber where Self: SettingsAttachedScreen {
+    
+    var selected_num: Int {
+        settings.state100.selectedNum
+    }
+    
+    var selectedNumBadgeItem: UIBarButtonItem {
+        let badgeView = BadgeSwift().then {
+            $0.text = "\(selected_num)首"
+            $0.insets = CGSize(width: 5, height: 5)
+            $0.font = UIFont.preferredFont(forTextStyle: .caption1)
+            $0.textColor = .white
+            $0.badgeColor = .systemRed
+            $0.sizeToFit()
+        }
+        return UIBarButtonItem(customView: badgeView)
+    }
+}
