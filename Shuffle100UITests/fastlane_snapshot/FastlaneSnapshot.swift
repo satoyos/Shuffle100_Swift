ec4e0d53226be12417ecefc6dfb8b10478ef192a
Yoshifumi Sato <sato_yos@nifty.com>
2022-10-24 18:31:20 +0900

    Take 1st screenshot of 1st device: Succeed!!

--- Shuffle100UITests/fastlane_snapshot/FastlaneSnapshot.swift
+++ Shuffle100UITests/fastlane_snapshot/FastlaneSnapshot.swift
@@ -21,11 +21,26 @@ final class FastlaneSnapshot: XCTestCase {
     }
 
     
-    func test_ScreenShotExample() throws {
-        // 「歌を選ぶ」画面に遷移した状態のスクリーンショットを撮ってみる
-        homePage.goToPoemPickerPage()
-        XCTAssert(PoemPickerPage(app: app).exists)
-        snapshot("0_trial_screenshot")
+//    func test_ScreenShotExample() throws {
+//        // 「歌を選ぶ」画面に遷移した状態のスクリーンショットを撮ってみる
+//        homePage.goToPoemPickerPage()
+//        XCTAssert(PoemPickerPage(app: app).exists)
+//        snapshot("0_trial_screenshot")
+//    }
+    
+    func test_RecitePoemScreenShot() {
+        // given
+        let recitePage = homePage.gotoRecitePoemPage()
+        // when
+        recitePage.forwardButton.tap()
+        // then
+        XCTAssert(recitePage.isReciting(number: 1, side: .kami))
+        // when
+        sleep(2)
+        recitePage.playButton.tap()
+        // then
+        XCTAssert(recitePage.isWaitinfForPlay)
+        snapshot("1_RecitePoemScreen")
     }
 
 }
