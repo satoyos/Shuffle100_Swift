55f1d6ef0ab6c09523c5db260265bf5d941ae477
Yoshifumi Sato <sato_yos@nifty.com>
2022-11-26 19:54:28 +0900

    Use common UITest code among BeginnerMode tests and HokkaidoMode tests

--- Shuffle100UITests/UITestWIthMultiScreen/HokkaidoModeUITest.swift
+++ Shuffle100UITests/UITestWIthMultiScreen/HokkaidoModeUITest.swift
@@ -20,40 +20,9 @@ final class HokkaidoModeUITest: XCTestCase {
     }
 
    
-    func test_selectHokkaidoMode() throws {
-        setHokkaidoMode()
-    }
-    
     func test_startGameInHokkaidoMode() {
-        // given
-        setHokkaidoMode()
-        // when
-        let recitePage = homePage.gotoRecitePoemPage()
-        // then
-        XCTAssert(recitePage.isRecitingJoka)
-        // when
-        recitePage.tapForwardButton()
-        // then
-        XCTAssert(recitePage.isReciting(number: 1, side: .shimo))
-        // when
-        recitePage.tapForwardButton()
-        // then
-        let whatsNextPage = WhatsNextpage(app: app)
+        let whatsNextPage = homePage.skipToWhatsNextPage(mode: .hokkaido)
         XCTAssert(whatsNextPage.exists)
     }
-    
-    private func setHokkaidoMode() {
-        // when
-        let selectModePage = homePage.gotoSelectModePage()
-        // then
-        XCTAssert(selectModePage.exists)
-        // when
-        selectModePage
-            .selectMode(.hokkaido)
-            .backToTopButton.tap()
-        // then
-        XCTAssert(homePage.reciteModeIs(.hokkaido))
-    }
-
 }
 #endif
--- Shuffle100UITests/pages/protocols/SkipToWhatsNextProtocol.swift
+++ Shuffle100UITests/pages/protocols/SkipToWhatsNextProtocol.swift
@@ -10,29 +10,35 @@ import Foundation
 import XCTest
 
 protocol SkipToWhatsNext {
-    func skipToWhatsNextPage(totalPoemsNum: Int) -> WhatsNextpage
+    func skipToWhatsNextPage(totalPoemsNum: Int, mode: ReciteMode) -> WhatsNextpage
 }
 
 extension SkipToWhatsNext where Self: HomePage  {
     @discardableResult
-    func skipToWhatsNextPage(totalPoemsNum: Int = 100) -> WhatsNextpage {
+    func skipToWhatsNextPage(totalPoemsNum: Int = 100, mode: ReciteMode = .beginner) -> WhatsNextpage {
+        // check
+        if !(mode == .beginner || mode == .hokkaido) {
+            XCTFail("Reciting mode must be .beginner or .hokkaido")
+        }
         // when
         let reciteModePage = gotoSelectModePage()
         reciteModePage
-            .selectMode(.beginner)
+            .selectMode(mode)
             .backToTopButton.tap()
         // then
-        XCTAssert(reciteModeIs(.beginner), "初心者モードになっている")
+        XCTAssert(reciteModeIs(mode), "選んだモードになっている")
         // when
         let recitePage = gotoRecitePoemPage()
         // then
         XCTAssert(recitePage.jokaTitle.exists, "序歌の読み上げ画面に到達")
         // when
         recitePage.tapForwardButton()
-        // then
-        XCTAssert(recitePage.isReciting(number: 1, side: .kami, total: totalPoemsNum))
-        // when
-        recitePage.tapForwardButton()
+        if mode == .beginner {
+            // then
+            XCTAssert(recitePage.isReciting(number: 1, side: .kami, total: totalPoemsNum))
+            // when
+            recitePage.tapForwardButton()
+        }
         // then
         XCTAssert(recitePage.isReciting(number: 1, side: .shimo, total: totalPoemsNum))
         // when
