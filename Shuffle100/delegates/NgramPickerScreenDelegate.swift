ed36f56ff8cb9661463ff3a41de996d8ce3f794d
Yoshifumi Sato <sato_yos@nifty.com>
2023-03-04 00:17:50 +0900

    Refactor: integrate almost same 2 methods to one

--- Shuffle100/delegates/NgramPickerScreenDelegate.swift
+++ Shuffle100/delegates/NgramPickerScreenDelegate.swift
@@ -11,31 +11,28 @@ import UIKit
 extension NgramPickerScreen: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let tappedCell = cellForIndexPath(indexPath)
-        switch tappedCell.selectedStatus {
-        case .full:
-            makeSelectedNone(cell: tappedCell)
-        default:
-            makeSelectedFull(cell: tappedCell)
-        }
+        let id = tappedCell.accessibilityLabel!
+        guard let numbers = numbersDic[id] else { fatalError("「\(id)」に対応する歌番号の配列が見つかりません")}
+        let newState100 =
+        flippedState(from: tappedCell.selectedStatus,
+                 for: numbers)
+        settings.state100 = newState100
+        updateTableAndBadge()
     }
     
     internal func cellForIndexPath(_ indexPath: IndexPath) -> NgramPickerTableCell {
         return tableView(tableView, cellForRowAt: indexPath) as! NgramPickerTableCell
     }
     
-    private func makeSelectedFull(cell: NgramPickerTableCell) {
-        let id = cell.accessibilityLabel!
-        guard let numbers = numbersDic[id] else { fatalError("「\(id)」に対応する歌番号の配列が見つかりません")}
-        let newState100 = settings.state100.selectInNumbers(numbers)
-        settings.state100 = newState100
-        updateTableAndBadge()
-    }
-    
-    private func makeSelectedNone(cell: NgramPickerTableCell) {
-        let id = cell.accessibilityLabel!
-        guard let numbers = numbersDic[id] else { fatalError("「\(id)」に対応する歌番号の配列が見つかりません")}
-        let newState100 = settings.state100.cancelInNumbers(numbers)
-        settings.state100 = newState100
-        updateTableAndBadge()
+    private func flippedState(
+        from selectedState: PoemsSelectedState,
+        for numbers: [Int]) -> SelectedState100 {
+            
+        switch selectedState {
+        case .full:
+            return settings.state100.cancelInNumbers(numbers)
+        default:
+            return settings.state100.selectInNumbers(numbers)
+        }
     }
 }
