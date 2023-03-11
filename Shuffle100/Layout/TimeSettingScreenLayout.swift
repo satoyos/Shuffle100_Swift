b251df95102ed1af584009238b61a64dd4f487cc
Yoshifumi Sato <sato_yos@nifty.com>
2023-03-11 11:41:15 +0900

    modify some private methods to computed variables in TimeSettingScreenLayout

--- Shuffle100/Layout/TimeSettingScreenLayout.swift
+++ Shuffle100/Layout/TimeSettingScreenLayout.swift
@@ -20,12 +20,16 @@ extension TimeSettingScreen {
     private func configureTimeLabel() {
         _ = timeLabel.then {
             $0.text = "0.00"
-            $0.font = UIFont.monospacedDigitSystemFont(ofSize: labelPointSize(), weight: .medium)
+            $0.font =
+                UIFont.monospacedDigitSystemFont(
+                    ofSize: labelPointSize,
+                    weight: .medium)
             $0.sizeToFit()
             $0.snp.makeConstraints{ (make) -> Void in
                 // Center => [50%, 40%]
                 make.centerX.equalToSuperview()
-                make.centerY.equalToSuperview().offset(-1 * one10thOfViewHeight())
+                make.centerY.equalToSuperview()
+                    .offset(-1 * one10thOfViewHeight)
             }
         }
     }
@@ -38,7 +42,8 @@ extension TimeSettingScreen {
             $0.sizeToFit()
             $0.snp.makeConstraints{ (make) -> Void in
                 make.centerX.equalToSuperview()
-                make.top.equalTo(slider.snp.bottom).offset(0.5 * one10thOfViewHeight())
+                make.top.equalTo(slider.snp.bottom)
+                    .offset(0.5 * one10thOfViewHeight)
             }
         }
     }
@@ -46,42 +51,42 @@ extension TimeSettingScreen {
     private func configureSlider() {
         _ = slider.then {
             $0.snp.makeConstraints{ (make) -> Void in
-                make.width.equalTo(0.8 * viewWidth())
-                make.height.equalTo(sliderHeight())
+                make.width.equalTo(0.8 * viewWidth)
+                make.height.equalTo(sliderHeight)
                 make.centerX.equalToSuperview()
-                make.top.equalTo(timeLabel.snp.bottom).offset(blankBetweenLabelAndSlider())
+                make.top.equalTo(timeLabel.snp.bottom)
+                    .offset(blankBetweenLabelAndSlider)
             }
             $0.minimumValue = minIntervalDuration
             $0.maximumValue = maxIntervalDuration
-//            $0.value = settings.interval
             $0.value = initialTime
             $0.accessibilityLabel = "slider"
         }
         updateTimeLabel()
     }
 
-    private func labelPointSize() -> CGFloat {
+    private var labelPointSize: CGFloat {
         sizeByDevice.intervalTimeLabelPointSize
     }
     
-    private func one10thOfViewHeight() -> CGFloat {
-        0.1 * viewHeiht()
+    private var one10thOfViewHeight: CGFloat {
+        0.1 * viewHeiht
     }
     
-    private func blankBetweenLabelAndSlider() -> CGFloat {
-        return sliderHeight()
+    private var blankBetweenLabelAndSlider: CGFloat {
+        sliderHeight
     }
 
-    private func sliderHeight() -> CGFloat {
+    private var sliderHeight: CGFloat {
         sizeByDevice.intervalSiderHeight
     }
     
-    private func viewWidth() -> CGFloat {
-        return self.view.frame.size.width
+    private var viewWidth: CGFloat {
+        view.frame.size.width
     }
     
-    private func viewHeiht() -> CGFloat {
-        return view.frame.size.height
+    private var viewHeiht: CGFloat {
+        view.frame.size.height
     }
 
 }
