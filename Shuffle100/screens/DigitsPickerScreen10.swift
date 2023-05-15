59a92f87845e6eb2b92540d1967cf76eb36e593b
Yoshifumi Sato <sato_yos@nifty.com>
2023-05-15 20:35:37 +0900

    Just introduced DigitsPickerScreen10

---
+++ Shuffle100/screens/DigitsPickerScreen10.swift
@@ -0,0 +1,15 @@
+//
+//  DigitsPickerScreen10.swift
+//  Shuffle100
+//
+//  Created by Yoshifumi Sato on 2023/05/15.
+//  Copyright © 2023 里 佳史. All rights reserved.
+//
+
+import UIKit
+
+final class DigitsPickerScreen10: SettingsAttachedScreen {
+    override func viewDidLoad() {
+        self.title = "10の位の数で選ぶ"
+    }
+}
---
+++ Shuffle100Tests/screens/DigitsPickerScreen10Test.swift
@@ -0,0 +1,22 @@
+//
+//  DigitsPickerScreen10Test.swift
+//  Shuffle100Tests
+//
+//  Created by Yoshifumi Sato on 2023/05/15.
+//  Copyright © 2023 里 佳史. All rights reserved.
+//
+
+import XCTest
+
+final class DigitsPickerScreen10Test: XCTestCase {
+
+    func test_initialScreen() {
+        // given
+        let screen = DigitsPickerScreen10()
+        // when
+        screen.loadViewIfNeeded()
+        // then
+        XCTAssertEqual(screen.title,"10の位の数で選ぶ")
+    }
+
+}
