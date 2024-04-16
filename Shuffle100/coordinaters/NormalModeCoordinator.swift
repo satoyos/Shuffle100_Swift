d5b68e1ffeb556fc56176e2507f3560d706089e7
Yoshifumi Sato <sato_yos@nifty.com>
2024-04-16 17:55:31 +0900

    Use new mothod to create deck

--- Shuffle100/coordinaters/NormalModeCoordinator.swift
+++ Shuffle100/coordinaters/NormalModeCoordinator.swift
@@ -20,7 +20,7 @@ final class NormalModeCoordinator: Coordinator, RecitePoemProtocol {
         self.navigationController = navigationController
         self.settings = settings
         self.store = store
-        let deck = Poem100.createFrom(state100: settings.state100)
+        let deck = settings.state100.convertToDeck()
         self.poemSupplier = PoemSupplier(deck: deck, shuffle: true)
         if settings.fakeMode {
             poemSupplier.addFakePoems()
