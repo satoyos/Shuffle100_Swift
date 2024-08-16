4a57dde41c672c6285cf811459df3e9d4ea3357b
Yoshifumi Sato <sato_yos@nifty.com>
2024-08-16 11:51:17 +0900

    Update AudioPlayerFactory to load fake autio file at the first time

--- Shuffle100/audioPlayers/AudioPlayerFactory.swift
+++ Shuffle100/audioPlayers/AudioPlayerFactory.swift
@@ -31,29 +31,21 @@ class AudioPlayerFactory {
           // それでも失敗するようなら、どうしようもない。
           //
 //          assertionFailure("AudioSessionの初期設定時エラー: \(error)")
+            
         }
+        // 初回再生時のロードによる遅延を回避するために、一度カラ準備しておく。
+        _ = AudioPlayerFactory.shared.preparePlayer(folder: "audio/misc", file: "250-milliseconds-of-silence", ofType: "mp3", title: "無音(250ms)")
     }
     
     func prepareOpeningPlayer(folder: String) -> AVAudioPlayer {
-//        let player: AVAudioPlayer
-//
-//        guard let path = Bundle.main.path(forResource: folder + "/序歌", ofType: "m4a") else {
-//            fatalError("音源ファイルが見つかりません")
-//        }
-//        do {
-//            player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
-//
-//        } catch {
-//            fatalError("序歌のAudioPlayer生成に失敗しました。folder => [\(folder)]\n - Error: \(error.localizedDescription)")
-//        }
         let player = preparePlayer(folder: folder, file: "序歌", title: "序歌")
         return player
     }
     
-    func preparePlayer(folder: String, file: String, title: String?) -> AVAudioPlayer {
+    func preparePlayer(folder: String, file: String, ofType ext: String = "m4a", title: String?) -> AVAudioPlayer {
         let player: AVAudioPlayer
         
-        guard let path = Bundle.main.path(forResource: folder + "/" + file, ofType: "m4a") else {
+        guard let path = Bundle.main.path(forResource: folder + "/" + file, ofType: ext) else {
             fatalError("音源ファイルが見つかりません")
         }
         do {
@@ -62,21 +54,11 @@ class AudioPlayerFactory {
         } catch {
             fatalError("\(title ?? "音源")のAudioPlayer生成に失敗しました。folder => [\(folder)]\n - Error: \(error.localizedDescription)")
         }
+        player.prepareToPlay()
         return player
     }
     
     func preparePlayer(number: Int, side: Side, folder: String) -> AVAudioPlayer {
-//        let player: AVAudioPlayer
-//
-//        guard let path = Bundle.main.path(forResource: folder + "/" + String(format: "%03d", number) + tailForSide(side), ofType: "m4a") else {
-//            fatalError("音源ファイルが見つかりません")
-//        }
-//        do {
-//            player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
-//
-//        } catch {
-//            fatalError("歌番号[\(number)]のAudioPlayer生成に失敗しました。folder => [\(folder)]\n - Error: \(error.localizedDescription)")
-//        }
         let file = String(format: "%03d", number) + tailForSide(side)
         let title = "歌番号[\(number)]"
         let player = preparePlayer(folder: folder, file: file, title: title)
