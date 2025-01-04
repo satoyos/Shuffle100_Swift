63b9f9c6aa9746bc0387f59ce2e4ab4262d954a8
Yoshifumi Sato <sato_yos@nifty.com>
2025-01-04 20:18:24 +0900

    Introduce KamiShimoDurationSetting
    not yet replaced, just introduced

--- Shuffle100/SwiftUI Views/KamiShimoDurationSetting.swift
+++ Shuffle100/SwiftUI Views/KamiShimoDurationSetting.swift
@@ -8,12 +8,52 @@
 
 import SwiftUI
 
-struct KamiShimoDurationSetting: View {
+struct KamiShimoDurationSetting {
+    @ObservedObject private var viewModel: DurationSettingViewModel
+    @EnvironmentObject var screenSizeStore: ScreenSizeStore
+    let settings: Settings
+    
+    // To catch event: navigation back to Parent View of SwiftUI
+    @Environment(\.isPresented) private var isPresented
+    
+    init(durationType: DurationSettingType, startTime: Double, settings: Settings = .init()) {
+        self.viewModel = DurationSettingViewModel(durationType: .kamiShimo, startTime: startTime, singer: Singers.fetchSingerFrom(settings))
+        self.settings = settings
+    }
+}
+
+extension KamiShimoDurationSetting: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: digitSize / 4) {
+            Sec2F(digitSize: 100, viewModel: viewModel.timeViewModel)
+            Slider(value: viewModel.$binding.startTime, in: 0.5 ... 2.0, step: 0.02 )
+                .accessibilityIdentifier("slider")
+                .padding(.horizontal)
+                .disabled(viewModel.output.isUserActionDisabled)
+            Button("試しに聞いてみる") {
+                viewModel.input.startTrialCountDownRequest.send()
+            }
+            .buttonStyle(.borderedProminent)
+            .foregroundStyle(Color.white)
+            .padding(.top)
+            .disabled(viewModel.output.isUserActionDisabled)
+        }
+        .onChange(of: isPresented) {
+            guard !isPresented else { return }
+            reflectSliderValueToSettings()
+        }
+    }
+    
+    func reflectSliderValueToSettings() {
+        settings.kamiShimoInterval = Float(viewModel.binding.startTime)
+    }
+    
+    private var digitSize: Double {
+        screenSizeStore.screenWidth / 5.0
     }
 }
 
 #Preview {
-    KamiShimoDurationSetting()
+    KamiShimoDurationSetting(durationType: .kamiShimo,  startTime: 1.0)
+        .environmentObject(ScreenSizeStore())
 }
