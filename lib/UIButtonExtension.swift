54274c790ed77ea9989322fdc12ada742ec5d5a3
Yoshifumi Sato <sato_yos@nifty.com>
2025-02-21 18:47:06 +0900

    Modify indent of source files for Libs: 2 -> 4

--- lib/UIButtonExtension.swift
+++ lib/UIButtonExtension.swift
@@ -9,15 +9,15 @@
 import UIKit
 
 extension UIButton {
-    func setStandardTitleColor() {
-        self.setTitleColor(UIColor(displayP3Red: 0.0, green: 0.5, blue: 1.0, alpha: 1.0), for: .normal)
-        self.setTitleColor(UIColor(displayP3Red: 0.0, green: 0.5, blue: 1.0, alpha: 0.3), for: .highlighted)
-        self.setTitleColor(StandardColor.disabledButtonColor, for: .disabled)
-    }
-    
-    func setImageWithStarndardColor(_ image: UIImage) {
-        self.setImage(image.tint(color: .systemIndigo), for: .normal)
-        self.setImage(image.tint(color: .systemIndigo), for: .normal)
-    }
+  func setStandardTitleColor() {
+    self.setTitleColor(UIColor(displayP3Red: 0.0, green: 0.5, blue: 1.0, alpha: 1.0), for: .normal)
+    self.setTitleColor(UIColor(displayP3Red: 0.0, green: 0.5, blue: 1.0, alpha: 0.3), for: .highlighted)
+    self.setTitleColor(StandardColor.disabledButtonColor, for: .disabled)
+  }
+  
+  func setImageWithStarndardColor(_ image: UIImage) {
+    self.setImage(image.tint(color: .systemIndigo), for: .normal)
+    self.setImage(image.tint(color: .systemIndigo), for: .normal)
+  }
 }
 
