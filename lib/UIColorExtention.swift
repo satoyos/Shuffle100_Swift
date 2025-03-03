2c59d4289433365ff7ff88c64f0355f7e930f663
Yoshifumi Sato <sato_yos@nifty.com>
2025-03-03 20:45:18 +0900

    delete @availables for old days

--- lib/UIColorExtention.swift
+++ lib/UIColorExtention.swift
@@ -23,16 +23,13 @@ extension UIColor {
   
   /// ライト/ダーク用の色を受け取ってDynamic Colorを作って返す
   public class func dynamicColor(light: UIColor, dark: UIColor) -> UIColor {
-    if #available(iOS 13, *) {
-      return UIColor { (traitCollection) -> UIColor in
-        if traitCollection.userInterfaceStyle == .dark {
-          return dark
-        } else {
-          return light
-        }
+    UIColor { (traitCollection) -> UIColor in
+      if traitCollection.userInterfaceStyle == .dark {
+        return dark
+      } else {
+        return light
       }
     }
-    return light
   }
 }
 
