1bc45bbe2a8e9b92f84f46e0cab975b1c75950ed
Yoshifumi Sato <sato_yos@nifty.com>
2025-05-03 22:49:14 +0900

    Import Swift UI Views for FiveColors

---
+++ Shuffle100/SwiftUI Views/FiveColorsView.swift
@@ -0,0 +1,78 @@
+//
+//  FiveColorsView.swift
+//  Shuffle100
+//
+//  Created by Yoshifumi Sato on 2025/04/01.
+//
+
+import SwiftUI
+
+struct FiveColorsView {
+  let settings: Settings
+  @State private var showActionSheet = false
+  @State private var selectedColor: FiveColors = .blue
+  @ObservedObject private var viewModel: FiveColorsViewModel
+  @EnvironmentObject var screenSizeStore: ScreenSizeStore
+  @Environment(\.isPresented) private var isPresented
+  
+  init(settings: Settings) {
+    self.settings = settings
+    self.viewModel = .init(state100: settings.state100)
+  }
+}
+
+extension FiveColorsView: View {
+  var body: some View {
+    NavigationStack {
+      Spacer()
+      List {
+        ForEach(FiveColors.all) { color in
+          FiveColorButton(viewModel: color.buttonViewModel) {
+            showActionSheet = true
+            selectedColor = color
+          }
+          .actionSheet(isPresented: $showActionSheet,
+                       content: fiveColorsActionSheet)
+        }
+      }
+      .toolbar {
+        ToolbarItem(placement: .confirmationAction) {
+          BadgeView(number: viewModel.output.state100.selectedNum)
+        }
+        ToolbarItem(placement: .principal) {
+          Text("五色百人一首")
+        }
+      }
+    }
+    .onChange(of: isPresented) {
+      guard !isPresented else { return }
+      tasksForLeavingThisVIew()
+    }
+  }
+  
+  private func fiveColorsActionSheet() -> ActionSheet {
+    ActionSheet(
+      title: Text("\(selectedColor)色の20首をどうしますか？"),
+      message: nil,
+      buttons: [
+        .default(Text("この20首だけを選ぶ")) {
+          viewModel.input.selectJust20OfColor.send(selectedColor)
+        },
+        .default(Text("今選んでいる札に加える")) {
+          viewModel.input.add20OfColor.send(selectedColor)
+        },
+        .cancel()
+      ]
+    )
+  }
+  
+  func tasksForLeavingThisVIew() {
+    settings.state100 = viewModel.output.state100
+  }
+  
+}
+
+#Preview {
+  FiveColorsView(settings: Settings())
+    .environmentObject(ScreenSizeStore())
+}
