2b61ade2740f51b882496fd960cf2c5a8e852ca9
Yoshifumi Sato <sato_yos@nifty.com>
2025-05-04 20:20:41 +0900

    Connect FiveColorView with its coordinator

--- Shuffle100UITests/pages/FiveColorsPage.swift
+++ Shuffle100UITests/pages/FiveColorsPage.swift
@@ -29,7 +29,8 @@ final class FiveColorsPage: PageObjectable {
     }
     
     func badge(of number: Int) -> XCUIElement {
-        app.navigationBars.staticTexts["\(number)首"]
+//        app.navigationBars.staticTexts["\(number)首"]
+      app.staticTexts["\(number)首"]
     }
     
     private func colorButton(of color: FiveColors) -> XCUIElement {
