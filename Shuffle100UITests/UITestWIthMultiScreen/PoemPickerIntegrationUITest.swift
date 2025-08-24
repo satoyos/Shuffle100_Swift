b889f32faa2c7d63ff2624b2eff85e41f9700efa
Yoshifumi Sato <sato_yos@nifty.com>
2025-08-24 13:52:33 +0900

    Update cellOf(number:) method of PoamPickerPage, to make PoemPickerIntegrationUITest pass

--- Shuffle100UITests/UITestWIthMultiScreen/PoemPickerIntegrationUITest.swift
+++ Shuffle100UITests/UITestWIthMultiScreen/PoemPickerIntegrationUITest.swift
@@ -9,93 +9,93 @@
 import XCTest
 
 final class PoemPickerIntegrationUITest: XCTestCase {
-
-    var app = XCUIApplication()
-    lazy var homePage = HomePage(app: app)
-    
-    override func setUp() {
-        super.setUp()
-        continueAfterFailure = false
-        app.launchArguments.append("--uitesting")
-        app.launch()
-        self.homePage = HomePage(app: app)
+  
+  var app = XCUIApplication()
+  lazy var homePage = HomePage(app: app)
+  
+  override func setUp() {
+    super.setUp()
+    continueAfterFailure = false
+    app.launchArguments.append("--uitesting")
+    app.launch()
+    self.homePage = HomePage(app: app)
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
+  
+  func test_HomeScreenReflectsSelectioninPoemPicker() {
+    XCTContext.runActivity(named: "デフォルトのトップ画面に「100首」と書かれたセルが存在する") { (activity) in
+      XCTAssert(homePage.numberOfSelecttedPoems(is: 100))
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    let poemPickerPage = homePage.goToPoemPickerPage()
+    XCTAssert(poemPickerPage.exists, "「歌を選ぶ」画面に到達")
+    XCTAssert(poemPickerPage.searchField.exists, "プレースホルダ付きの検索窓がある")
+    XCTContext.runActivity(named: "歌を3つタップして選択状態を解除し、トップ画面に戻ると、歌の数が97首になっている") { (activity) in
+      //   tap poems #1,2, 4
+      poemPickerPage
+        .tapCellof(number: 1)
+        .tapCellof(number: 2)
+        .tapCellof(number: 4)
+        .backToTopPage()
+      // then
+      XCTAssert(homePage.numberOfSelecttedPoems(is: 97))
     }
+  }
+  
+  func test_searchPoem() {
+    // when
+    let poemPickerPage = homePage.goToPoemPickerPage()
     
-    func test_HomeScreenReflectsSelectioninPoemPicker() {        
-        XCTContext.runActivity(named: "デフォルトのトップ画面に「100首」と書かれたセルが存在する") { (activity) in
-            XCTAssert(homePage.numberOfSelecttedPoems(is: 100))
-        }
-        let poemPickerPage = homePage.goToPoemPickerPage()
-        XCTAssert(poemPickerPage.exists, "「歌を選ぶ」画面に到達")
-        XCTAssert(poemPickerPage.searchField.exists, "プレースホルダ付きの検索窓がある")
-        XCTContext.runActivity(named: "歌を3つタップして選択状態を解除し、トップ画面に戻ると、歌の数が97首になっている") { (activity) in
-            //   tap poems #1,2, 4
-            poemPickerPage
-                .tapCellof(number: 1)
-                .tapCellof(number: 2)
-                .tapCellof(number: 4)
-                .backToTopPage()
-            // then
-            XCTAssert(homePage.numberOfSelecttedPoems(is: 97))
-        }
+    XCTContext.runActivity(named: "検索窓に「あき」を入力すると、検索用データがそれにヒットする歌のみ表示される"){
+      _ in
+      // when
+      let searchField = poemPickerPage.searchField
+      searchField.tap()
+      searchField.typeText("あき")
+      // then
+      XCTAssert(poemPickerPage.cellOf(number: 1).exists)
+      XCTAssertFalse(poemPickerPage.cellOf(number: 2).exists)
+      XCTAssert(poemPickerPage.cellOf(number: 5).exists)
     }
+    XCTContext.runActivity(named: "検索結果としてフィルタリングされた歌をタップしたら、選択状態を変えることができる") { _ in
+      // when
+      poemPickerPage
+        .tapCellof(number: 1)
+        .tapCellof(number: 5)
+        .cancelButton.tap()
+      //   back to HomeScreen
+      poemPickerPage.backToTopButton.tap()
+      // then
+      XCTAssert(homePage.numberOfSelecttedPoems(is: 98))
+    }
+  }
+  
+  func test_alertAppearsWhenStartGameWithoutSelectedPoem() {
+    // given
+    let poemPickerPage = homePage.goToPoemPickerPage()
+    // when
+    poemPickerPage.cancelAllButton.tap()
+    poemPickerPage.backToTopButton.tap()
+    // then
+    XCTAssert(homePage.numberOfSelecttedPoems(is: 0))
     
-    func test_searchPoem() {
-        // when
-        let poemPickerPage = homePage.goToPoemPickerPage()
-        
-        XCTContext.runActivity(named: "検索窓に「あき」を入力すると、検索用データがそれにヒットする歌のみ表示される"){
-            (activity) in
-            // when
-            let searchField = poemPickerPage.searchField
-            searchField.tap()
-            searchField.typeText("あき")
-            // then
-            XCTAssert(poemPickerPage.cellOf(number: 1).exists)
-            XCTAssertFalse(poemPickerPage.cellOf(number: 2).exists)
-            XCTAssert(poemPickerPage.cellOf(number: 5).exists)
-        }
-        XCTContext.runActivity(named: "検索結果としてフィルタリングされた歌をタップしたら、選択状態を変えることができる") { (acitvity) in
-            // when
-            poemPickerPage
-                .tapCellof(number: 1)
-                .tapCellof(number: 5)
-                .cancelButton.tap()
-            //   back to HomeScreen
-            poemPickerPage.backToTopButton.tap()
-            // then
-            XCTAssert(homePage.numberOfSelecttedPoems(is: 98))
-        }
+    XCTContext.runActivity(named: "歌を全く選ばないまま試合を開始しようとすると、アラートが表示される") { _ in
+      // when
+      homePage.gameStartCell.tap()
+      // then
+      XCTAssert(homePage.noPoemSelectedAlert.exists)
     }
-    
-    func test_alertAppearsWhenStartGameWithoutSelectedPoem() {
-        // given
-        let poemPickerPage = homePage.goToPoemPickerPage()
-        // when
-        poemPickerPage.cancelAllButton.tap()
-        poemPickerPage.backToTopButton.tap()
-        // then
-        XCTAssert(homePage.numberOfSelecttedPoems(is: 0))
-
-        XCTContext.runActivity(named: "歌を全く選ばないまま試合を開始しようとすると、アラートが表示される") { activity in
-            // when
-            homePage.gameStartCell.tap()
-            // then
-            XCTAssert(homePage.noPoemSelectedAlert.exists)
-        }
-        XCTContext.runActivity(named: "アラート画面で「戻る」ボタンを押すと、アラートは消える") { acivity in
-            // when
-            homePage.dismissAlert()
-            // then
-            XCTAssertFalse(homePage.noPoemSelectedAlert.exists)
-        }
-        XCTContext.runActivity(named: "アラート画面が消えてトップ画面に戻ったとき、「試合開始」セルの選択は解除された状態になっている") { activity in
-            XCTAssertFalse(homePage.gameStartCell.isSelected)
-        }
+    XCTContext.runActivity(named: "アラート画面で「戻る」ボタンを押すと、アラートは消える") { acivity in
+      // when
+      homePage.dismissAlert()
+      // then
+      XCTAssertFalse(homePage.noPoemSelectedAlert.exists)
     }
+    XCTContext.runActivity(named: "アラート画面が消えてトップ画面に戻ったとき、「試合開始」セルの選択は解除された状態になっている") { activity in
+      XCTAssertFalse(homePage.gameStartCell.isSelected)
+    }
+  }
 }
 
--- Shuffle100UITests/pages/PoemPickerPage.swift
+++ Shuffle100UITests/pages/PoemPickerPage.swift
@@ -10,152 +10,148 @@ import Foundation
 import XCTest
 
 final class PoemPickerPage: PageObjectable, WaitInUITest {
-    let app: XCUIApplication
-    
-    init(app: XCUIApplication) {
-        self.app = app
-    }
-    
-    var pageTitle: XCUIElement {
-        app.navigationBars[A11y.title].firstMatch
-    }
-    
-    var backToTopButton: XCUIElement {
-        app.navigationBars.buttons[A11y.backToTop].firstMatch
-    }
-    
-    var searchField: XCUIElement {
-        app.searchFields[A11y.searchFieldPlaceHolder].firstMatch
-    }
-    
-    var cancelButton: XCUIElement {
-        app.buttons[A11y.cancel].firstMatch
-    }
-    
-    var cancelAllButton: XCUIElement {
-        waitToHittable(for: app.buttons[A11y.cancelAll], timeout: timeOutSec)
-    }
-    
-    var selectAllButton: XCUIElement {
-        waitToHittable(for: app.toolbars.buttons[A11y.selectAll], timeout: timeOutSec)
-    }
-    
-    var selectByGroupButton: XCUIElement {
-        waitToHittable(for: app.toolbars.buttons[A11y.selectByGroup].firstMatch, timeout: timeOutSec)
-    }
-    
-    var saveButton: XCUIElement {
-        app.navigationBars.buttons[A11y.save].firstMatch
-    }
-    
-    var firstCellInList: XCUIElement {
-        app.cells.firstMatch
-    }
-    
-    enum A11y {
-        static let title = "歌を選ぶ"
-        static let backToTop = "トップ"
-        static let searchFieldPlaceHolder = "歌を検索"
-        static let cancel = "キャンセル"
-        static let cancelAll = "全て取消"
-        static let selectAll = "全て選択"
-        static let selectByGroup = "まとめて選ぶ"
-        static let save = "保存"
-    }
-    
-    func badge(of number: Int) -> XCUIElement {
-        app.navigationBars.staticTexts["\(number)首"]
-    }
-    
-    @discardableResult
-    func tapCellof(number: Int) -> Self {
-        let a11yId = String(format: "%03d", number)
-        let cell = app.cells[a11yId].firstMatch
-        XCTAssert(cell.exists)
-        cell.tap()
-        return self
-    }
-    
-    @discardableResult
-    func tapDetailButtonOf(number: Int) -> TorifudaPage {
-      app.buttons["detail_\(number)"].firstMatch.tap()
-      return TorifudaPage(app: app)
-    }
-    
-    @discardableResult
-    func tapDetailButton(of cell: XCUIElement) -> TorifudaPage {
-      cell.buttons.firstMatch.tap()
-        return TorifudaPage(app: app)
-    }
-    
-    @discardableResult
-    func backToTopPage() -> HomePage {
-        backToTopButton.tap()
-        let homePage = HomePage(app: app)
-        XCTAssert(homePage.exists, "トップ画面に戻った")
-        return homePage
-    }
-    
-    func cellOf(number: Int) -> XCUIElement {
-//        let label = String(format: "%03d", number)
-//        return app.tables.cells[label].firstMatch
-      app.otherElements.containing(.button, identifier: "detail_\(number)").firstMatch
-    }
-    
-    func longPress(number: Int) {
-        cellOf(number: number).press(forDuration: 2.0)
-    }
-    
-    @discardableResult
-    func gotoNgramPickerPage() -> NgramPickerPage {
-        let sheet = showSelectByGroupActionSheet()
-        sheet.selectByNgramButton.tap()
-        return NgramPickerPage(app: app)
-    }
-    
-    @discardableResult
-    func gotoDigitsPickerPage01() -> DigitsPickerPage01 {
-        let sheet = showSelectByGroupActionSheet()
-        sheet.selectByDigits01Button.tap()
-        return DigitsPickerPage01(app: app)
-    }
-    
-    @discardableResult
-    func gotoDigitsPickerPage10() -> DigitsPickerPage10 {
-        let sheet = showSelectByGroupActionSheet()
-        sheet.selectByDigits10Button.tap()
-        return DigitsPickerPage10(app: app)
-    }
-    
-    func showSelectByGroupActionSheet() -> SelectByGroupActionSheet {
-        selectByGroupButton.tap()
-        return SelectByGroupActionSheet(app: app)
-    }
-    
-    @discardableResult
-    func saveCurrentPoemsAsNewSet(name: String) -> Self {
-        // when
-        saveButton.tap()
-        let sheet = SaveFudaSetActionSheet(app: app)
-        let button = waitToHittable(for: sheet.saveNewFudaSetButton, timeout: timeOutSec)
-        button.tap()
-        // then
-        let alert = NameNewFudaSetAlert(app: app)
-        XCTAssert(alert.exists, "新しい札セットを命名するダイアログが現れる")
-        // when
-        alert.textField.tap()
-        alert.textField.typeText(name)
-        alert.confirmButton.tap()
-        // then
-        let compAlert = SaveNewSetCompletedAlert(app: app)
-        XCTAssert(compAlert.exists, "保存が完了した旨のダイアログが現れる")
-        // when
-        compAlert.confirmButton.tap()
-        // then
-        XCTAssertFalse(compAlert.exists, "確認ダイアログは消えている")
-        // and
-        return self
-    }
-    
-    
+  let app: XCUIApplication
+  
+  init(app: XCUIApplication) {
+    self.app = app
+  }
+  
+  var pageTitle: XCUIElement {
+    app.navigationBars[A11y.title].firstMatch
+  }
+  
+  var backToTopButton: XCUIElement {
+    app.navigationBars.buttons[A11y.backToTop].firstMatch
+  }
+  
+  var searchField: XCUIElement {
+    app.searchFields[A11y.searchFieldPlaceHolder].firstMatch
+  }
+  
+  var cancelButton: XCUIElement {
+    app.buttons[A11y.cancel].firstMatch
+  }
+  
+  var cancelAllButton: XCUIElement {
+    waitToHittable(for: app.buttons[A11y.cancelAll], timeout: timeOutSec)
+  }
+  
+  var selectAllButton: XCUIElement {
+    waitToHittable(for: app.toolbars.buttons[A11y.selectAll], timeout: timeOutSec)
+  }
+  
+  var selectByGroupButton: XCUIElement {
+    waitToHittable(for: app.toolbars.buttons[A11y.selectByGroup].firstMatch, timeout: timeOutSec)
+  }
+  
+  var saveButton: XCUIElement {
+    app.navigationBars.buttons[A11y.save].firstMatch
+  }
+  
+  var firstCellInList: XCUIElement {
+    app.cells.firstMatch
+  }
+  
+  enum A11y {
+    static let title = "歌を選ぶ"
+    static let backToTop = "トップ"
+    static let searchFieldPlaceHolder = "歌を検索"
+    static let cancel = "キャンセル"
+    static let cancelAll = "全て取消"
+    static let selectAll = "全て選択"
+    static let selectByGroup = "まとめて選ぶ"
+    static let save = "保存"
+  }
+  
+  func badge(of number: Int) -> XCUIElement {
+    app.navigationBars.staticTexts["\(number)首"]
+  }
+  
+  @discardableResult
+  func tapCellof(number: Int) -> Self {
+    cellOf(number: number).tap()
+    return self
+  }
+  
+  @discardableResult
+  func tapDetailButtonOf(number: Int) -> TorifudaPage {
+    app.buttons["detail_\(number)"].firstMatch.tap()
+    return TorifudaPage(app: app)
+  }
+  
+  @discardableResult
+  func tapDetailButton(of cell: XCUIElement) -> TorifudaPage {
+    cell.buttons.firstMatch.tap()
+    return TorifudaPage(app: app)
+  }
+  
+  @discardableResult
+  func backToTopPage() -> HomePage {
+    backToTopButton.tap()
+    let homePage = HomePage(app: app)
+    XCTAssert(homePage.exists, "トップ画面に戻った")
+    return homePage
+  }
+  
+  func cellOf(number: Int) -> XCUIElement {
+//    app.otherElements.containing(.button, identifier: "detail_\(number)").firstMatch
+    app.cells.containing(.button, identifier: "detail_\(number)").firstMatch
+  }
+  
+  func longPress(number: Int) {
+    cellOf(number: number).press(forDuration: 2.0)
+  }
+  
+  @discardableResult
+  func gotoNgramPickerPage() -> NgramPickerPage {
+    let sheet = showSelectByGroupActionSheet()
+    sheet.selectByNgramButton.tap()
+    return NgramPickerPage(app: app)
+  }
+  
+  @discardableResult
+  func gotoDigitsPickerPage01() -> DigitsPickerPage01 {
+    let sheet = showSelectByGroupActionSheet()
+    sheet.selectByDigits01Button.tap()
+    return DigitsPickerPage01(app: app)
+  }
+  
+  @discardableResult
+  func gotoDigitsPickerPage10() -> DigitsPickerPage10 {
+    let sheet = showSelectByGroupActionSheet()
+    sheet.selectByDigits10Button.tap()
+    return DigitsPickerPage10(app: app)
+  }
+  
+  func showSelectByGroupActionSheet() -> SelectByGroupActionSheet {
+    selectByGroupButton.tap()
+    return SelectByGroupActionSheet(app: app)
+  }
+  
+  @discardableResult
+  func saveCurrentPoemsAsNewSet(name: String) -> Self {
+    // when
+    saveButton.tap()
+    let sheet = SaveFudaSetActionSheet(app: app)
+    let button = waitToHittable(for: sheet.saveNewFudaSetButton, timeout: timeOutSec)
+    button.tap()
+    // then
+    let alert = NameNewFudaSetAlert(app: app)
+    XCTAssert(alert.exists, "新しい札セットを命名するダイアログが現れる")
+    // when
+    alert.textField.tap()
+    alert.textField.typeText(name)
+    alert.confirmButton.tap()
+    // then
+    let compAlert = SaveNewSetCompletedAlert(app: app)
+    XCTAssert(compAlert.exists, "保存が完了した旨のダイアログが現れる")
+    // when
+    compAlert.confirmButton.tap()
+    // then
+    XCTAssertFalse(compAlert.exists, "確認ダイアログは消えている")
+    // and
+    return self
+  }
+  
+  
 }
