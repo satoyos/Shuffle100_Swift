4b5ebea0fffe06f49b86051c49f229c2f5993581
Yoshifumi Sato <sato_yos@nifty.com>
2025-12-17 18:17:29 +0900

    Delete UIKit version files

--- Shuffle100/dataSources/SelectSingerScreenDataSource.swift
+++ Shuffle100/dataSources/SelectSingerScreenDataSource.swift
@@ -1,25 +1,25 @@
+////
+////  SelectSingerScreenDataSource.swift
+////  Shuffle100
+////
+////  Created by Yoshifumi Sato on 2020/03/11.
+////  Copyright © 2020 里 佳史. All rights reserved.
+////
 //
-//  SelectSingerScreenDataSource.swift
-//  Shuffle100
+//import UIKit
 //
-//  Created by Yoshifumi Sato on 2020/03/11.
-//  Copyright © 2020 里 佳史. All rights reserved.
-//
-
-import UIKit
-
-extension SelectSingerScreen: UIPickerViewDataSource {
-  
-  func numberOfComponents(in pickerView: UIPickerView) -> Int {
-    1
-  }
-  
-  func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-    Singers.all.count
-  }
-  
-  func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-    Singers.all[row].name
-  }
-  
-}
+//extension SelectSingerScreen: UIPickerViewDataSource {
+//  
+//  func numberOfComponents(in pickerView: UIPickerView) -> Int {
+//    1
+//  }
+//  
+//  func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+//    Singers.all.count
+//  }
+//  
+//  func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+//    Singers.all[row].name
+//  }
+//  
+//}
